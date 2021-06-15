2e6bb20ebd426d2ceb56e5d9ae810169c3572f0f
Woonjang Ahn <hodol@woowahan.com>
2021-06-16 00:53:11 +0900

    feat: add logger for signup and login events

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,12 +6,18 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
+    private static final Logger fileLog = LoggerFactory.getLogger("file");
+
     private MemberRepository memberRepository;
     private JwtTokenProvider jwtTokenProvider;
 
@@ -21,11 +27,18 @@ public class AuthService {
     }
 
     public TokenResponse login(TokenRequest request) {
-        Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(AuthorizationException::new);
-        member.checkPassword(request.getPassword());
-
-        String token = jwtTokenProvider.createToken(request.getEmail());
-        return new TokenResponse(token);
+        try {
+            Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(AuthorizationException::new);
+            member.checkPassword(request.getPassword());
+            log.info("Member #{} ({}) has logged in.", member.getId(), member.getEmail());
+            fileLog.info("Member #{} ({}) has logged in.", member.getId(), member.getEmail());
+            String token = jwtTokenProvider.createToken(request.getEmail());
+            return new TokenResponse(token);
+        } catch (AuthorizationException exception) {
+            log.warn("Login failed ({})", request.getEmail());
+            fileLog.warn("Login failed ({})", request.getEmail());
+            throw exception;
+        }
     }
 
     public LoginMember findMemberByToken(String credentials) {
--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -1,15 +1,23 @@
 package nextstep.subway.member.application;
 
+import nextstep.subway.auth.application.AuthorizationException;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
+    private static final Logger log = LoggerFactory.getLogger(MemberService.class);
+    private static final Logger fileLog = LoggerFactory.getLogger("file");
+
     private MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
@@ -17,8 +25,20 @@ public class MemberService {
     }
 
     public MemberResponse createMember(MemberRequest request) {
-        Member member = memberRepository.save(request.toMember());
-        return MemberResponse.of(member);
+        try {
+            Member member = memberRepository.save(request.toMember());
+            log.info("Member #{} ({}) has been created.", member.getId(), member.getEmail());
+            fileLog.info("Member #{} has been created.", member.getId());
+            return MemberResponse.of(member);
+        } catch (IllegalArgumentException exception) {
+            log.warn("MemberRequest contains null value.", exception);
+            fileLog.warn("MemberRequest contains null value.", exception);
+            throw new AuthorizationException();
+        } catch (DataAccessException exception) {
+            log.warn("Something wrong during creating member.", exception);
+            fileLog.warn("Something wrong during creating member.", exception);
+            throw new AuthorizationException();
+        }
     }
 
     public MemberResponse findMember(Long id) {
