8ecef9d01df09e9ddeb13221ec6dbfbb992b6fbb
Park Sang Yun <71218974+pa324@users.noreply.github.com>
2021-06-16 00:05:03 +0900

    Step1 - 인프라 운영하기 (#4)

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -1,16 +1,23 @@
 package nextstep.subway.auth.ui;
 
-import nextstep.subway.auth.application.AuthService;
-import nextstep.subway.auth.dto.TokenRequest;
-import nextstep.subway.auth.dto.TokenResponse;
+import static net.logstash.logback.argument.StructuredArguments.kv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import nextstep.subway.auth.application.AuthService;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+
 @RestController
 public class AuthController {
     private AuthService authService;
+    private static final Logger log = LoggerFactory.getLogger("console");
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
 
     public AuthController(AuthService authService) {
         this.authService = authService;
@@ -19,6 +26,9 @@ public class AuthController {
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
         TokenResponse token = authService.login(request);
+        log.info("["+request.getEmail()+"] 로그인");
+        fileLogger.info("["+request.getEmail()+"] 로그인");
+        jsonLogger.info("{}", kv("loginEmail", request.getEmail()));
         return ResponseEntity.ok().body(token);
     }
 }
--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -2,14 +2,19 @@ package nextstep.subway.map.ui;
 
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @RestController
 public class MapController {
     private MapService mapService;
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
 
     public MapController(MapService mapService) {
         this.mapService = mapService;
@@ -17,6 +22,9 @@ public class MapController {
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
+        PathResponse pathResponse = mapService.findPath(source, target);
+        jsonLogger.info("{} {}", kv("stations", pathResponse.getStations()),
+                kv("distance", pathResponse.getDistance()));
         return ResponseEntity.ok(mapService.findPath(source, target));
     }
 }
