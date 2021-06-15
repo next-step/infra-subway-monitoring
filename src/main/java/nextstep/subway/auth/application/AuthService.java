3ae14ac079330e96065c8b2abd9c56f7fcd55f25
hyunsung.kim <hsboee@gmail.com>
2021-06-15 23:48:05 +0900

    [1단계 - 인프라 운영하기] 김현성 리뷰 부탁드립니다. (#7)

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -4,14 +4,19 @@ import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
+import nextstep.subway.member.application.MemberService;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
+
     private MemberRepository memberRepository;
     private JwtTokenProvider jwtTokenProvider;
 
@@ -23,7 +28,7 @@ public class AuthService {
     public TokenResponse login(TokenRequest request) {
         Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(AuthorizationException::new);
         member.checkPassword(request.getPassword());
-
+        log.info("login - id:{} email:{}", member.getId(), member.getEmail());
         String token = jwtTokenProvider.createToken(request.getEmail());
         return new TokenResponse(token);
     }
--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -7,14 +7,19 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 import java.util.List;
 
 @Service
 @Transactional
 public class MapService {
+    private static final Logger log = LoggerFactory.getLogger("json");
+
     private LineService lineService;
     private StationService stationService;
     private PathService pathService;
@@ -30,7 +35,10 @@ public class MapService {
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
-
+        log.info("{}, {}",
+            kv("출발지", sourceStation.getName()),
+            kv("도착지", targetStation.getName())
+        );
         return PathResponseAssembler.assemble(subwayPath);
     }
 }
