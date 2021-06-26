71cf8cdf8235c3b991418c5a2c94a7ea4e062129
Jiseon Lee <leejoyc9310@gmail.com>
2021-06-27 00:00:31 +0900

    refactor : LogAop 리팩토링

--- src/main/java/nextstep/subway/common/LogAop.java
+++ src/main/java/nextstep/subway/common/LogAop.java
@@ -18,74 +18,86 @@ public class LogAop {
     private static final Logger fileLogger = LoggerFactory.getLogger("file");
     private static final Logger consoleLogger = LoggerFactory.getLogger("console");
 
+    private static final String ERROR_LOG_FORMAT = "[CLASS] {} / [METHOD] {} / [ERROR] {}";
+    private static final String REQUEST_INFO_LOG_FORMAT = "[CLASS] {} / [METHOD] {} / [REQUEST] {}";
+    private static final String RESPONSE_INFO_LOG_FORMAT = "[CLASS] {} / [METHOD] {} / [RESPONSE] {}";
+
     @Around("execution(* nextstep.subway.auth.application.AuthService.login(..))")
     public Object loginLogging(ProceedingJoinPoint joinPoint) throws Throwable {
-        String inputParam = Arrays.toString(joinPoint.getArgs());
-        consoleLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[REQUEST]" + inputParam);
+        requestInfoLog(joinPoint);
 
-        //메소드 실행
-        Object result = joinPoint.proceed();
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+        } catch (Exception e) {
+            resultErrorLog(joinPoint, e);
+        }
 
-        //메소드 실행결과 로그
-        consoleLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[RESPONSE]" + result);
+        responseInfoLog(joinPoint, result);
         return result;
     }
 
     @Around("execution(* nextstep.subway.member.application.MemberService.createMember(..))")
     public Object signUpLogging(ProceedingJoinPoint joinPoint) throws Throwable {
 
-        String inputParam = Arrays.toString(joinPoint.getArgs());
-        consoleLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[REQUEST]" + inputParam);
-        fileLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[REQUEST]" + inputParam);
+        requestInfoLog(joinPoint);
 
-        //메소드 실행
-        Object result = joinPoint.proceed();
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+        } catch (Exception e) {
+            resultErrorLog(joinPoint, e);
+        }
 
-        //메소드 실행결과 로그
-        consoleLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[RESPONSE]" + result);
-        fileLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[RESPONSE]" + result);
+        responseInfoLog(joinPoint, result);
         return result;
 
     }
 
     @Around("execution(* nextstep.subway.map.application.MapService.findPath(..))")
     public Object findPathLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+        requestInfoLog(joinPoint);
 
-        String inputParam = Arrays.toString(joinPoint.getArgs());
-        consoleLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[REQUEST]" + inputParam);
-        fileLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[REQUEST]" + inputParam);
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+        } catch (Exception e) {
+            resultErrorLog(joinPoint, e);
+        }
 
-        //메소드 실행
-        Object result = joinPoint.proceed();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-        String output = gson.toJson(result);
-        //메소드 실행결과 로그
-        consoleLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[RESPONSE]"
-                + "\n" + output);
-        fileLogger.info("[CLASS] " + joinPoint.getSignature().getDeclaringTypeName() + "/"
-                + "[METHOD] " + joinPoint.getSignature().getName() + "/"
-                + "[RESPONSE]" + result);
-
+        responseInfoLog(joinPoint, result);
         return result;
 
+    }
+
+    private void responseInfoLog(ProceedingJoinPoint joinPoint, Object result) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String output = gson.toJson(result);
+
+        consoleLogger.info(RESPONSE_INFO_LOG_FORMAT,
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                output);
+    }
+
+    private void requestInfoLog(ProceedingJoinPoint joinPoint) {
+        String inputParam = Arrays.toString(joinPoint.getArgs());
+
+        consoleLogger.info(REQUEST_INFO_LOG_FORMAT,
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                inputParam);
+    }
+
+    private void resultErrorLog(ProceedingJoinPoint joinPoint, Exception e) {
+        consoleLogger.info(ERROR_LOG_FORMAT,
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                e.toString());
+
+        fileLogger.info(ERROR_LOG_FORMAT,
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                e.toString());
     }
 }
