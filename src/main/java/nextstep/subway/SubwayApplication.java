332a0685a3093ef515afcd7ed1fe03b935743d79
lenkim <joenggyu0@gmail.com>
2021-06-26 21:06:39 +0900

    refactor: 메트릭 테스트를 위한 코드 제거

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -2,8 +2,6 @@ package nextstep.subway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,18 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @EnableJpaAuditing
 @SpringBootApplication
-public class SubwayApplication implements ApplicationRunner {
+public class SubwayApplication {
 
     private static Logger log = LoggerFactory.getLogger(SubwayApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(SubwayApplication.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
-        for (int i = 0; i < 1000; i++) {
-            log.info("SubwayApplication.run > i");
-        }
     }
 }
