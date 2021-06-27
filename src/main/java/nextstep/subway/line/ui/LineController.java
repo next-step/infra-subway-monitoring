42080c74c347d0ca970f25ec85c49e07c2cd6d63
seokjin <7271kim@naver.com>
2021-06-27 16:35:17 +0900

    feature : [STEP1] Thread dump 확인 시 deadlock, 과도한 cpu 사용을 잡고 있는 코드 수정

--- src/main/java/nextstep/subway/line/ui/LineController.java
+++ src/main/java/nextstep/subway/line/ui/LineController.java
@@ -1,17 +1,27 @@
 package nextstep.subway.line.ui;
 
-import nextstep.subway.line.application.LineService;
-import nextstep.subway.line.dto.LineRequest;
-import nextstep.subway.line.dto.LineResponse;
-import nextstep.subway.line.dto.SectionRequest;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.util.List;
 import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.line.dto.SectionRequest;
 
 @RestController
 @RequestMapping("/lines")
@@ -69,14 +79,13 @@ public class LineController {
 
     static final Object left = new Object();
     static final Object right = new Object();
+
     @GetMapping("/lock-left")
     public String findLockLeft() throws InterruptedException {
 
         synchronized (left) {
             Thread.sleep(5000);
-            synchronized (right) {
-                System.out.println("left");
-            }
+            System.out.println("left");
         }
         return "ok";
     }
@@ -85,9 +94,7 @@ public class LineController {
     public String findLockRight() throws InterruptedException {
         synchronized (right) {
             Thread.sleep(5000);
-            synchronized (left) {
-                System.out.println("right");
-            }
+            System.out.println("right");
         }
         return "ok";
     }
@@ -101,11 +108,7 @@ public class LineController {
     }
 
     private IntUnaryOperator extracted(double value) {
-        while (value >= 0) {
-            value = Math.tan(value);
-        }
-        return null;
+        return operand -> (int) Math.tan(value);
     }
-
 
 }
