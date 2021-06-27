8ea97e882c8ec3ab5447efb74ceaa0106203f104
yjs2952 <yjs2952@naver.com>
2021-06-27 16:02:05 +0900

    refactor: AOP 로깅 Filter 로 변경

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -2,9 +2,11 @@ package nextstep.subway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ServletComponentScan
 @EnableJpaRepositories
 @EnableJpaAuditing
 @SpringBootApplication
