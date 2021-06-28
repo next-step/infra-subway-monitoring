ae77085fa79cc72d39737d2cc28a6ef192630226
Len kim <joenggyu0@gmail.com>
2021-06-28 11:39:36 +0900

    [Step1] 로깅과 모니터링 (#33)
    * feat: 애플리케이션 진단 실습시 발생되는 문제가 되는 코드 제거
    
    * feat: prod profile 생성
    
    * feat: logback.xml 설정
    
    * fix: logback.xml 에서 prod 일 때, 파일에 저장되지 않는 이슈 해결
    
    * refactor: 주요 이벤트에 로그 남겨지도록 설정
    
    * feat: cloudwatch 설정과 설정 테스트 코드 추가
    
    * fix: logback의 파일 저장소 위치 변경
    
    * feat: logback file 생성 위치 변경
    
    * refactor: 메트릭 테스트를 위한 코드 제거
    
    * fix: 잘못 설정한 Prod Profile 수정
    
    * refactor: logback.xml 제거후, 각 profile 에서 설정
    
    * feat: logback.xml 제거
    
    * docs: 작업내역 체크함
    
    * refactor: 로그의 형식을 toString()을 활용하는 방향으로 변경함

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -1,5 +1,7 @@
 package nextstep.subway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,8 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class SubwayApplication {
 
+    private static Logger log = LoggerFactory.getLogger(SubwayApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(SubwayApplication.class, args);
     }
-
 }
