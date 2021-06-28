f357be1f3023231b7ef06888316b62990178ebfa
ybh89 <akass191@gmail.com>
2021-06-28 19:39:28 +0900

    feat: 로그 파일 잘못딘 위치 수정, response dto toString 재정의

--- src/main/java/nextstep/subway/member/dto/MemberResponse.java
+++ src/main/java/nextstep/subway/member/dto/MemberResponse.java
@@ -31,4 +31,11 @@ public class MemberResponse {
     public Integer getAge() {
         return age;
     }
+
+    @Override
+    public String toString() {
+        return "MemberResponse{" +
+                "id=" + id +
+                '}';
+    }
 }
