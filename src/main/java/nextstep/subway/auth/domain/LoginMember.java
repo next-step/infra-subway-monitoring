ca7b6d768055abc0cb0eb0d0024c9c9e761c4e97
Jung Insup <jisjis1218@gmail.com>
2021-06-29 17:24:35 +0900

    refactor(개인정보 삭제): DTO에 개인정보(email, pw 등) 삭제

--- src/main/java/nextstep/subway/auth/domain/LoginMember.java
+++ src/main/java/nextstep/subway/auth/domain/LoginMember.java
@@ -22,13 +22,4 @@ public class LoginMember {
     public Integer getAge() {
         return age;
     }
-
-    @Override
-    public String toString() {
-        return "LoginMember{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
