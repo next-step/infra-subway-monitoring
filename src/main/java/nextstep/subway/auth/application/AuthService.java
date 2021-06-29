1247637f4d7b037e344921d4fd3daacc73cc7f2c
ttuop <plintcsz@gmail.com>
2021-06-29 15:11:22 +0900

    feat: json 로그 기록

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -1,5 +1,12 @@
 package nextstep.subway.auth.application;
 
+import static net.logstash.logback.argument.StructuredArguments.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
@@ -7,18 +14,13 @@ import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 @Service
 @Transactional
 public class AuthService {
-    private static final Logger logger = LoggerFactory.getLogger(AuthService.class);
+    private static final Logger json = LoggerFactory.getLogger("json");
 
-    private MemberRepository memberRepository;
-    private JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public AuthService(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider) {
         this.memberRepository = memberRepository;
@@ -31,8 +33,8 @@ public class AuthService {
 
         String token = jwtTokenProvider.createToken(request.getEmail());
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("Member login. {}", member);
+        if (json.isDebugEnabled()) {
+            json.debug("{}", kv("memberId", member.getId()));
         }
 
         return new TokenResponse(token);
--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -1,22 +1,23 @@
 package nextstep.subway.member.application;
 
-import nextstep.subway.line.ui.LineController;
-import nextstep.subway.member.domain.Member;
-import nextstep.subway.member.domain.MemberRepository;
-import nextstep.subway.member.dto.MemberRequest;
-import nextstep.subway.member.dto.MemberResponse;
+import static net.logstash.logback.argument.StructuredArguments.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.member.domain.MemberRepository;
+import nextstep.subway.member.dto.MemberRequest;
+import nextstep.subway.member.dto.MemberResponse;
+
 @Service
 @Transactional
 public class MemberService {
-    private static final Logger logger = LoggerFactory.getLogger(MemberService.class);
+    private static final Logger json = LoggerFactory.getLogger("json");
 
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -25,8 +26,8 @@ public class MemberService {
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
 
-        if (logger.isInfoEnabled()) {
-            logger.info("Member created. {}", member);
+        if (json.isInfoEnabled()) {
+            json.info("{}", kv("memberId", member.getId()));
         }
 
         return MemberResponse.of(member);
