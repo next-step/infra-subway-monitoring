7ddf7eb0fb119faca800ca9b6bdc81ea99dc5fc5
DESKTOP-T6O4081\lee <lsecret@naver.com>
2021-07-02 21:06:23 +0900

    chore: logger 추가 및 로깅 추가
    logger 추가 후 로그 생성 소스 추가

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,14 +3,19 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 @RestController
 public class AuthController {
-    private AuthService authService;
+    private final AuthService authService;
+    private static final Logger json = LoggerFactory.getLogger("json");
 
     public AuthController(AuthService authService) {
         this.authService = authService;
@@ -19,6 +24,9 @@ public class AuthController {
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
         TokenResponse token = authService.login(request);
+
+        json.info("{}", kv("accessToken", token.getAccessToken()));
+
         return ResponseEntity.ok().body(token);
     }
 }
--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -7,6 +7,8 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +17,12 @@ import java.util.List;
 @Service
 @Transactional
 public class MapService {
-    private LineService lineService;
-    private StationService stationService;
-    private PathService pathService;
+    private final LineService lineService;
+    private final StationService stationService;
+    private final PathService pathService;
+
+    private static final Logger log = LoggerFactory.getLogger("console");
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
 
     public MapService(LineService lineService, StationService stationService, PathService pathService) {
         this.lineService = lineService;
@@ -31,6 +36,10 @@ public class MapService {
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
 
+        log.info("SourceStation Name : {}",  sourceStation.getName());
+        log.info("TargetStation Name : {}",  targetStation.getName());
+        fileLogger.info("SourceStation Name : {}",  sourceStation.getName());
+        fileLogger.info("TargetStation Name : {}",  targetStation.getName());
         return PathResponseAssembler.assemble(subwayPath);
     }
 }
