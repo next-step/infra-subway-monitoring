15966b91c95eda1c978cd28c7f942fe6ee1298f7
dohchoi91 <dohchoi91@gmail.com>
2021-07-03 21:16:16 +0900

    2단계 성능 테스트 완료

---
+++ script/smoke.js
@@ -0,0 +1,81 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+  vus: 1, // 1 user looping for 1 minute
+  duration: '10s',
+
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const BASE_URL = 'http://3.34.50.208:8080';
+const USERNAME = 'test@abc.com';
+const PASSWORD = '1';
+export function index() {
+	return http.get(`${BASE_URL}`);
+}
+
+export function login() {
+  let payload = JSON.stringify({
+    email: USERNAME,
+    password: PASSWORD,
+  });
+
+  let params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  return http.post(`${BASE_URL}/login/token`, payload, params);
+};
+
+export function getMyInfo(loginRes) {
+  let authHeaders = {
+    headers: {
+      Authorization: `Bearer ${loginRes.json('accessToken')}`
+    },
+  };
+  return http.get(`${BASE_URL}/members/me`, authHeaders).json();
+};
+
+export function updateMyInfo(loginRes) {
+  let payload = JSON.stringify({
+    email: USERNAME,
+    password: PASSWORD,
+    age: '20'
+  });
+
+  let params = {
+    headers: {
+      Authorization: `Bearer ${loginRes.json('accessToken')}`,
+    },
+  };
+  return http.put(`${BASE_URL}/members/me`, payload, params).json();
+};
+
+export function findPath(loginRes, source, target) {
+  let params = {
+    headers: {
+      Authorization: `Bearer ${loginRes.json('accessToken')}`,
+    }
+  };
+
+  return http.get(`${BASE_URL}/paths/?source=` + source + `&target=` + target, params).json();
+};
+
+export default function() {
+  index();
+
+  let loginRes = login();
+	check(loginRes, {"logged in successfully": resp => resp.json("accessToken") !== ""});
+  
+	check(getMyInfo(loginRes), { "retrieved member": obj => obj.id != 0 });
+	
+  check(updateMyInfo(loginRes), { "updated info": obj => obj.id != 0 });
+
+	check(findPath(loginRes, 2, 11), { "path check": obj => obj.stations.length != 0 });
+
+	sleep(1);
+}
