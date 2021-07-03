2ec1e0e3819c0f62b6d50af7ea2a6add45f51e26
jordy-torvalds <jordy-torvalds@kakao.com>
2021-07-04 06:30:13 +0900

    feat: 로그인 로그 적

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,13 +3,19 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.hibernate.annotations.common.util.impl.LoggerFactory.logger;
+
 @RestController
 public class AuthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     private AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -18,7 +24,9 @@ public class AuthController {
 
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
+        logger.info("로그인 요청: {}", request);
         TokenResponse token = authService.login(request);
+        logger.info("로그인 응답: {}", token);
         return ResponseEntity.ok().body(token);
     }
 }
