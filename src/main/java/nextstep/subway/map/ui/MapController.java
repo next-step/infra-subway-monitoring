75149a3df0e104302e9de67afa040de490da8cc6
fDevJc <fDevJc@gmail.com>
2021-07-04 16:26:50 +0900

    feat: 회원가입, 로그인, 최단거리 조회 이벤트 로깅 설정

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -2,6 +2,8 @@ package nextstep.subway.map.ui;
 
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
+    private static final Logger logger = LoggerFactory.getLogger(MapController.class);
     private MapService mapService;
 
     public MapController(MapService mapService) {
@@ -17,6 +20,9 @@ public class MapController {
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
-        return ResponseEntity.ok(mapService.findPath(source, target));
+        logger.info("경로 조회 요청: source = {} , target = {}",source,target);
+        PathResponse pathResponse = mapService.findPath(source, target);
+        logger.info("경로 조회 응답: {}",pathResponse);
+        return ResponseEntity.ok(pathResponse);
     }
 }
