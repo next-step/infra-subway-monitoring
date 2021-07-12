751d80ea75ac47f6e4aca82382291b2d874e200f
JisuLim <sanola3@gmail.com>
2021-07-13 01:40:35 +0900

    feat: add logging

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,6 +3,8 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    private final Logger log = LoggerFactory.getLogger("file");
+
     private AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -18,6 +22,7 @@ public class AuthController {
 
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
+        log.info("로그인이 시도 되었습니다. email: " + request.getEmail());
         TokenResponse token = authService.login(request);
         return ResponseEntity.ok().body(token);
     }
--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -2,6 +2,8 @@ package nextstep.subway.map.ui;
 
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
+    private final Logger log = LoggerFactory.getLogger("file");
+
     private MapService mapService;
 
     public MapController(MapService mapService) {
@@ -17,6 +21,7 @@ public class MapController {
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
+        log.info("최단거리 조회가 요청되었습니다. sourceStationId: " + source + " targetStationId: " + target);
         return ResponseEntity.ok(mapService.findPath(source, target));
     }
 }
