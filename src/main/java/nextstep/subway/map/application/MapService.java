54f0219d53456244d552b19554095dc61d75d92b
chajs226 <chajs226@gmail.com>
2021-07-12 22:35:27 +0900

    feat: 로깅설정

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -7,6 +7,8 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,8 @@ import java.util.List;
 @Service
 @Transactional
 public class MapService {
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+
     private LineService lineService;
     private StationService stationService;
     private PathService pathService;
@@ -30,6 +34,10 @@ public class MapService {
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
+
+        fileLogger.info("{}, {}",
+                "출발지: " + sourceStation.getName(),
+                "도착지: " + targetStation.getName());
 
         return PathResponseAssembler.assemble(subwayPath);
     }
