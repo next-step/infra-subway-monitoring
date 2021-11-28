67e89f9f6bf91a058a93089dfaee5298079f1363
HeeSeok Noh <nhs0912@nate.com>
2021-11-28 15:08:49 +0900

    feat: log파일 설정 추가

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -1,5 +1,7 @@
 package nextstep.subway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,7 +12,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class SubwayApplication {
 
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(SubwayApplication.class);
+        logger.info("안녕하세요!!! INFO입니다");
+
+        fileLogger.info("파일 로깅입니다!!");
         SpringApplication.run(SubwayApplication.class, args);
     }
 
