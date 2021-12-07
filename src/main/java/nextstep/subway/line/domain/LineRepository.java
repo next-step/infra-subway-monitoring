bfbf86bc790c42cf31ee169d47599d6de76d4695
IW.MOON <mooniw02@gmail.com>
2021-12-07 23:25:38 +0900

    fix: 문제코드 수정

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
 
-    @Query(value = "SELECT * FROM line WHERE SLEEP(3)", nativeQuery = true)
+   // @Query(value = "SELECT * FROM line WHERE SLEEP(3)", nativeQuery = true)
     List<Line> findAll();
 }
