310aacbbbdc7cef8063abe84fa0c4ff4e69c18a6
ChangSubKwak <paradise1732@gmail.com>
2021-12-12 00:35:00 +0900

    feat: AspectJ의 AOP 로깅 기능을 사용하기 위한 커스텀 어노테이션(@AspectLogging) 추가

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -14,6 +14,7 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @AspectLogging
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
         TokenResponse token = authService.login(request);
