f0d9ab2c5f0e700b39c5660bc36d11856ada6a50
Chaeyun Lee <chaeyunn@iochord.com>
2021-12-11 22:39:58 +0900

    feat: DTO toString 설정

--- src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
+++ src/main/java/nextstep/subway/favorite/dto/FavoriteResponse.java
@@ -4,32 +4,41 @@ import nextstep.subway.favorite.domain.Favorite;
 import nextstep.subway.station.dto.StationResponse;
 
 public class FavoriteResponse {
-    private Long id;
-    private StationResponse source;
-    private StationResponse target;
+	private Long id;
+	private StationResponse source;
+	private StationResponse target;
 
-    public FavoriteResponse() {
-    }
+	public FavoriteResponse() {
+	}
 
-    public FavoriteResponse(Long id, StationResponse source, StationResponse target) {
-        this.id = id;
-        this.source = source;
-        this.target = target;
-    }
+	public FavoriteResponse(Long id, StationResponse source, StationResponse target) {
+		this.id = id;
+		this.source = source;
+		this.target = target;
+	}
 
-    public static FavoriteResponse of(Favorite favorite, StationResponse source, StationResponse target) {
-        return new FavoriteResponse(favorite.getId(), source, target);
-    }
+	public static FavoriteResponse of(Favorite favorite, StationResponse source, StationResponse target) {
+		return new FavoriteResponse(favorite.getId(), source, target);
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public StationResponse getSource() {
-        return source;
-    }
+	public StationResponse getSource() {
+		return source;
+	}
 
-    public StationResponse getTarget() {
-        return target;
-    }
+	public StationResponse getTarget() {
+		return target;
+	}
+
+	@Override
+	public String toString() {
+		return "FavoriteResponse{" +
+			"id=" + id +
+			", source=" + source +
+			", target=" + target +
+			'}';
+	}
 }
