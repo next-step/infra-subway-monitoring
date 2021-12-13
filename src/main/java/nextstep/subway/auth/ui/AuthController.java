57af51005ef38346c974d531a40b7f36ebe5540c
Jeongkyo Kim <43088225+chapitak@users.noreply.github.com>
2021-12-13 14:50:46 +0900

    🚀 1단계 - 로깅과 모니터링 (#131)
    * feat: logback 설정파일을 추가한다
    
    * feat: 회원가입, 거리조회, 로그인 시 로깅을 남긴다
    
    * feat: logback-encoder에 대한 의존성을 추가한다
    
    * feat: 테스트 로그 추가
    
    * feat: 로깅 경로를 추가한다
    
    * docs: Cloudwatch 대시보드 URL을 추가한다
    
    * feat: Spring Actuator Metrics 수집 설정 추가한다

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,6 +3,8 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    public static final String MESSAGE_SIGN_IN = " 님이 로그인을 시도했습니다";
     private AuthService authService;
+    private Logger fileLogger = LoggerFactory.getLogger("file");
 
     public AuthController(AuthService authService) {
         this.authService = authService;
@@ -18,6 +22,7 @@ public class AuthController {
 
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
+        fileLogger.info(request.getEmail() + MESSAGE_SIGN_IN);
         TokenResponse token = authService.login(request);
         return ResponseEntity.ok().body(token);
     }
