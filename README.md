407d3fe57ef4f77f566bd831fac6670b47311137
Wangki Lee <lwk3007@gmail.com>
2021-12-13 20:37:46 +0900

    feature: 애플리케이션 진단하기 실습 진행 후 문제 되는 코드 수정

--- README.md
+++ README.md
@@ -46,6 +46,18 @@ npm run dev
 
 2. Cloudwatch 대시보드 URL을 알려주세요
 
+
+### 요구사항 
+- [x] 애플리케이션 진단하기 실습을 진행해보고 문제가 되는 코드를 수정
+- [ ] 로그 설정하기
+  - [ ] Application Log 파일로 저장하기
+    - [ ] 회원가입, 로그인, 최단거리 조회 등의 이벤트에 로깅을 설정
+  - [x] Nginx Access Log 설정하기 (설정 x)
+    - [x] ALB 로 대체
+- [ ] Cloudwatch 로 모니터링
+  - [ ] Cloudwatch 로 로그 수집하기
+  - [ ] Cloudwatch 로 메트릭 수집하기
+
 ---
 
 ### 2단계 - 성능 테스트
--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -1,5 +1,11 @@
 package nextstep.subway.line.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.line.domain.LineRepository;
 import nextstep.subway.line.dto.LineRequest;
@@ -7,17 +13,12 @@ import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class LineService {
-    private LineRepository lineRepository;
-    private StationService stationService;
+    private final LineRepository lineRepository;
+    private final StationService stationService;
 
     public LineService(LineRepository lineRepository, StationService stationService) {
         this.lineRepository = lineRepository;
@@ -27,7 +28,8 @@ public class LineService {
     public LineResponse saveLine(LineRequest request) {
         Station upStation = stationService.findById(request.getUpStationId());
         Station downStation = stationService.findById(request.getDownStationId());
-        Line persistLine = lineRepository.save(new Line(request.getName(), request.getColor(), upStation, downStation, request.getDistance()));
+        Line persistLine = lineRepository.save(
+            new Line(request.getName(), request.getColor(), upStation, downStation, request.getDistance()));
         return LineResponse.of(persistLine);
     }
 
@@ -45,7 +47,6 @@ public class LineService {
     public Line findLineById(Long id) {
         return lineRepository.findById(id).orElseThrow(RuntimeException::new);
     }
-
 
     public LineResponse findLineResponseById(Long id) {
         Line persistLine = findLineById(id);
