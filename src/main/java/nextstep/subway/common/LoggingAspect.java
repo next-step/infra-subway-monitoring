296185b804bcc41d82b1d891f83591ad35e3f84f
ch3224bin <ahrocksil@gmail.com>
2021-12-13 09:53:54 +0900

    feat : log를 Request 단위로 구별할 수 있게 trace.id 추가

--- src/main/java/nextstep/subway/common/LoggingAspect.java
+++ src/main/java/nextstep/subway/common/LoggingAspect.java
@@ -2,14 +2,14 @@ package nextstep.subway.common;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Aspect
 @Component
@@ -17,18 +17,25 @@ public class LoggingAspect {
     public static final String SUBWAY_UI_CONTROLLER = "execution(public * nextstep.subway..ui.*Controller.*(..))";
     public final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before(SUBWAY_UI_CONTROLLER)
+    @Pointcut(SUBWAY_UI_CONTROLLER)
+    public void controllerAdvice() {}
+
+
+    @Before("controllerAdvice()")
     public void before(JoinPoint joinPoint) {
+        MDC.put("trace.id", UUID.randomUUID().toString());
         log.info("[Request] - {}.{}() : {}", joinPoint.getTarget().getClass().getSimpleName(), joinPoint.getSignature().getName(), joinPoint.getArgs());
     }
 
-    @AfterReturning(value = SUBWAY_UI_CONTROLLER, returning = "result")
+    @AfterReturning(value = "controllerAdvice()", returning = "result")
     public void afterReturning(JoinPoint joinPoint, ResponseEntity<?> result) {
         log.info("[Response] - {}.{}() : {}", joinPoint.getTarget().getClass().getSimpleName(), joinPoint.getSignature().getName(), result.getBody());
+        MDC.clear();
     }
 
-    @AfterThrowing(value = SUBWAY_UI_CONTROLLER, throwing = "e")
+    @AfterThrowing(value = "controllerAdvice()", throwing = "e")
     public void afterThrowing(JoinPoint joinPoint, Exception e) {
         log.info("[Exception] - {}.{}() : {}", joinPoint.getTarget().getClass().getSimpleName(), joinPoint.getSignature().getName(), ExceptionUtils.getStackTrace(e));
+        MDC.clear();
     }
 }
