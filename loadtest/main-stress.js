d71dba09dcef5da020badbfc69d246457b517de1
Chaeyun Lee <chaeyunn@iochord.com>
2021-12-14 21:59:46 +0900

    docs: 테스트 계획 작성

---
+++ loadtest/main-stress.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import {sleep, check} from 'k6';
+
+export const options = {
+  stages: [
+    {duration: '10s', target: 20},
+    {duration: '10s', target: 50},
+    {duration: '20s', target: 100},
+    {duration: '10s', target: 150},
+    {duration: '20s', target: 200},
+    {duration: '10s', target: 100},
+    {duration: '10s', target: 0},
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<1000'],
+  },
+};
+
+const BASE_URL = 'https://chaeyun17.p-e.kr';
+
+export default function () {
+  const mainResponse = http.get(`${BASE_URL}`);
+  check(mainResponse, {
+    'load main page': response => response.status === 200
+  });
+  sleep(1);
+}
---
+++ loadtest/update-load.js
@@ -0,0 +1,44 @@
+import http from 'k6/http';
+import {check, sleep} from 'k6';
+
+export const options = {
+  stages: [
+    {duration: '5s', target: 50},
+    {duration: '10s', target: 100},
+    {duration: '20s', target: 100},
+    {duration: '10s', target: 0},
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<1000'],
+  },
+};
+
+const BASE_URL = 'https://chaeyun17.p-e.kr';
+const email = 'chaeyun17@github.com';
+const password = '12345';
+const params = {headers: {'Content-Type': 'application/json'}};
+const loginPayload = JSON.stringify({email, password});
+
+export default function () {
+  const loginResponse = http.post(`${BASE_URL}/login/token`,
+      loginPayload, params).json();
+  check(loginResponse, {
+    'logged in successfully': response => response.accessToken !== '',
+  });
+
+  const authHeaders = {
+    headers: {
+      Authorization: `Bearer ${loginResponse.accessToken}`,
+      'Content-Type': 'application/json',
+    }
+  };
+  const retrievedResponse = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+  check(retrievedResponse, {'retrieved member': obj => obj.id !== 0});
+
+  const updatedResponse = http.put(`${BASE_URL}/members/me`,
+      JSON.stringify({email, password, age: 10}), authHeaders);
+  check(updatedResponse,
+      {'updated member': response => response.status === 200}
+  );
+  sleep(1);
+}
