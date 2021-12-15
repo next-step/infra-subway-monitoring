efaa679e6ae5a550994152adabfec44a0650a168
vicjed <vicjed@icloud.com>
2021-12-16 00:00:03 +0900

    refactor(LogAop) : 로그출력 변경

--- src/main/java/nextstep/subway/common/logger/LogAop.java
+++ src/main/java/nextstep/subway/common/logger/LogAop.java
@@ -1,10 +1,13 @@
 package nextstep.subway.common.logger;
 
 
+import java.lang.reflect.Method;
 import javax.servlet.http.HttpServletRequest;
 import nextstep.subway.common.RequestUtils;
 import nextstep.subway.common.TraceId;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
@@ -19,7 +22,7 @@ public class LogAop {
 
     private static final Logger logger = LoggerFactory.getLogger(LogAop.class);
 
-    @Pointcut("within(nextstep.subway.*.ui.*)")
+    @Pointcut("within(nextstep.subway.*.ui.*Controller)")
     public void controller() {
     }
 
@@ -29,20 +32,32 @@ public class LogAop {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         try {
-            logger.debug("=======================================================");
-            logger.debug("REQUEST_URI: {}", request.getRequestURI());
-            logger.debug("METHOD_NAME: {}", request.getMethod());
-            logger.debug("PARAMETER: {}", RequestUtils.toParam(request));
-            logger.debug("TRACE_ID: {}", TraceId.of());
-            logger.debug("=======================================================");
+            logger.info("=======================================================");
+            logger.info("TRACE_ID: {}", TraceId.of());
+            logger.info("REQUEST_URI: {}", request.getRequestURI());
+            logger.info("METHOD_NAME: {}", request.getMethod());
+            logger.info("METHOD_SIGNATURE: {}", joinPoint.getSignature());
+            logger.info("PARAMETER {}", joinPoint.getArgs());
+            logger.info("=======================================================");
             Object proceed = joinPoint.proceed(joinPoint.getArgs());
             stopWatch.stop();
             return proceed;
         } catch (Exception e) {
+            logger.info("==================예외발생 START=================");
             logger.error(e.getMessage());
+            logger.info("==================예외발생  END =================");
             throw e;
         } finally {
-            logger.debug(stopWatch.prettyPrint());
+            logger.info(stopWatch.prettyPrint());
         }
+    }
+
+    @AfterReturning(value = "controller()", returning = "responseData")
+    public void afterReturnLog(JoinPoint joinPoint, Object responseData) {
+        // 메서드 정보 받아오기
+        logger.info("======= method name = {} =======", joinPoint.getSignature().getName());
+
+        logger.info("return type = {}", responseData.getClass().getSimpleName());
+        logger.info("return value = {}", responseData);
     }
 }
