91e2860fceca1ae87840eaefdffa60be2901175b
All-ForOne <ddabizz@gmail.com>
2021-12-15 22:16:22 +0900

    feat: 로그 / cloudwatch 설정

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -7,6 +7,8 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +17,10 @@ import java.util.List;
 @Service
 @Transactional
 public class MapService {
-    private LineService lineService;
-    private StationService stationService;
-    private PathService pathService;
+    private final LineService lineService;
+    private final StationService stationService;
+    private final PathService pathService;
+    private static final Logger pathLogger = LoggerFactory.getLogger("path");
 
     public MapService(LineService lineService, StationService stationService, PathService pathService) {
         this.lineService = lineService;
@@ -30,7 +33,7 @@ public class MapService {
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
-
+        pathLogger.info("경로가 조회되었습니다. 출발 : {0}, 도착 : {1}", source, target);
         return PathResponseAssembler.assemble(subwayPath);
     }
 }
--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,13 +4,16 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+    private static final Logger memberLogger = LoggerFactory.getLogger("member");
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -18,6 +21,7 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
+        memberLogger.info(member.getEmail() + "(" + member.getId() + ") 회원이 등록되었습니다.");
         return MemberResponse.of(member);
     }
 
@@ -29,9 +33,11 @@ public class MemberService {
     public void updateMember(Long id, MemberRequest param) {
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
         member.update(param.toMember());
+        memberLogger.info(member.getEmail() + "(" + member.getId() + ") 회원이 수정되었습니다.");
     }
 
     public void deleteMember(Long id) {
         memberRepository.deleteById(id);
+        memberLogger.info(id + " 회원이 삭제되었습니다.");
     }
 }
