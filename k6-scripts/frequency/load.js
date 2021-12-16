808fedd6b0653c7116dcb50ef44e9438c8c9bb9b
shinmj <gkdlqkek@gmail.com>
2021-12-16 10:02:32 +0900

    docs,feat: 부하 테스트 결과
    - README.md 부하테스트 결과 정리
    - script, 결과 이미지 추가

---
+++ k6-scripts/frequency/load.js
@@ -0,0 +1,62 @@
+import { check, sleep } from 'k6'
+import http from 'k6/http'
+
+/**
+ * 접속 빈도가 높은 페이지 - 메인
+ * 로그인 - 내정보 조회 - 즐겨 찾기 조회
+ *
+ * Throughput = 11.8 ~ 177
+ * Latency = 50~100ms
+ *
+ * vus
+ * 11.8 x (3x1.5 ) /3 = 18,
+ * 177 x (3x1.5)/3 = 266,
+ */
+
+export let options = {
+  stages: [
+    { duration: '5s', target: 18 },
+    { duration: '5s', target: 266 },
+    { duration: '20s', target: 18 },
+    { duration: '20s', target: 266 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+}
+
+const BASE_URL = 'https://shinmj-nextstep.n-e.kr'
+const USERNAME = 'shinmj@email.com'
+const PASSWORD = '1234'
+
+export default function () {
+  var payload = JSON.stringify({
+    email: USERNAME,
+    password: PASSWORD,
+  })
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  let loginRes = http.post(`${BASE_URL}/login/token`, payload, params)
+
+  check(loginRes, {
+    'logged in successfully': (resp) => resp.json('accessToken') !== '',
+  })
+
+  let authHeaders = {
+    headers: {
+      Authorization: `Bearer ${loginRes.json('accessToken')}`,
+    },
+  }
+  let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json()
+  check(myObjects, { 'retrieved member': (obj) => obj.id != 0 })
+  sleep(1)
+
+  let favorites = http.get(`${BASE_URL}/favorites`, authHeaders).json()
+  check(favorites, { 'retrieved favorite': (obj) => obj.id != 0 })
+  sleep(1)
+}
---
+++ k6-scripts/updated/README.md
@@ -0,0 +1,14 @@
+#  데이터를 갱신하는 페이지 
+- 노선정보 변경 (로그인 - 노선 목록 조회, 노선 정보 변경)
+
+## smoke test
+### [script](./smoke.js)
+![smoke.png](./smoke.png)
+
+## load test
+### [script](./load.js)
+![load.png](./load.png)
+
+## stress test
+### [script](./stress.js)
+![stress.png](./stress.png)
