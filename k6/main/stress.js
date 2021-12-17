92eaa2af4239923c895881e7606e38dc7e027a4e
김도영 <95doyoung@gmail.com>
2021-12-17 13:51:55 +0900

    refactor (k6) : 메인 스트레스 테스트 vus 수정 800 -> 2000

--- k6/main/stress.js
+++ k6/main/stress.js
@@ -1,17 +1,17 @@
 import http from 'k6/http';
-import {check, sleep} from 'k6';
+import {sleep} from 'k6';
 
 export const options = {
     stages: [
-        { duration: '30s', target: 100 }, // below normal load
-        { duration: '60s', target: 100 },
-        { duration: '30s', target: 400 }, // normal load
-        { duration: '60s', target: 400 },
-        { duration: '30s', target: 600 }, // around the breaking point
-        { duration: '60s', target: 600 },
-        { duration: '30s', target: 800 }, // beyond the breaking point 1
-        { duration: '60s', target: 800 },
-        { duration: '10s', target: 0 }, // scale down. Recovery stage.
+        {duration: '30s', target: 100}, // below normal load
+        {duration: '60s', target: 100},
+        {duration: '30s', target: 400}, // normal load
+        {duration: '60s', target: 400},
+        {duration: '30s', target: 600}, // around the breaking point
+        {duration: '60s', target: 600},
+        {duration: '30s', target: 2000}, // beyond the breaking point
+        {duration: '60s', target: 2000},
+        {duration: '10s', target: 0}, // scale down. Recovery stage.
     ],
 };
 
