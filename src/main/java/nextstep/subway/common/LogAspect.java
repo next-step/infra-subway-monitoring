7a617690c85e2ddfbaa652ff1c8e1178a6b757d0
Junhwan Kim <junhwan.tech@gmail.com>
2021-12-18 01:20:07 +0900

    chore: step1 피드백 반영, 로깅을 처리하는 방식 변경

--- src/main/java/nextstep/subway/common/LogAspect.java
+++ src/main/java/nextstep/subway/common/LogAspect.java
@@ -2,59 +2,57 @@ package nextstep.subway.common;
 
 import java.util.UUID;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogAspect {
-    private static final String TRACE_ID = "trace.id";
-    private static final Logger logger = LoggerFactory.getLogger("file");
 
-    @Pointcut("execution(* nextstep.subway.member.ui.MemberController.createMember(..))"
-        + " || execution(* nextstep.subway.auth.ui.AuthController.login(..))"
-        + " || execution(* nextstep.subway.map.ui.MapController.findPath(..))")
+    private static final String TRACE_ID = "trace.id";
+    private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+
+    @Pointcut("execution(* nextstep.subway.*.ui.*Controller.*(..))")
     public void controllerAdvice() {
     }
 
-    @Before("controllerAdvice()")
+    @Around("controllerAdvice()")
+    public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+        requestLogging(joinPoint);
+
+        Object result;
+
+        try {
+            result = joinPoint.proceed(joinPoint.getArgs());
+            responseLogging(result);
+        } catch (Exception e) {
+            logger.error("{} response error, {}", joinPoint.getSignature().getName(), e);
+            throw e;
+        } finally {
+            MDC.clear();
+        }
+
+        return result;
+    }
+
     public void requestLogging(JoinPoint joinPoint) {
         String traceId = UUID.randomUUID().toString();
         MDC.put(TRACE_ID, traceId);
 
         logger.info("{} request ", joinPoint.getSignature().getName());
 
-        for (Object args: joinPoint.getArgs()) {
+        for (Object args : joinPoint.getArgs()) {
             logger.info("args: {}", args);
         }
     }
 
-    @AfterReturning("controllerAdvice()")
-    public void responseLogging(JoinPoint joinPoint) {
-        logger.info("{} response", joinPoint.getSignature().getName());
-
-        for (Object args: joinPoint.getArgs()) {
-            logger.info("args: {}", args);
-        }
-
-        MDC.clear();
-    }
-
-    @AfterThrowing("controllerAdvice()")
-    public void responseErrorLogging(JoinPoint joinPoint) {
-        logger.error("{} response error", joinPoint.getSignature().getName());
-
-        for (Object args: joinPoint.getArgs()) {
-            logger.info("args: {}", args);
-        }
-
-        MDC.clear();
+    public void responseLogging(Object result) {
+        logger.info("response: {}", result);
     }
 }
--- src/main/java/nextstep/subway/member/ui/MemberController.java
+++ src/main/java/nextstep/subway/member/ui/MemberController.java
@@ -1,17 +1,27 @@
 package nextstep.subway.member.ui;
 
+import java.net.URI;
 import nextstep.subway.auth.domain.AuthenticationPrincipal;
 import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.member.application.MemberService;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
     private MemberService memberService;
 
     public MemberController(MemberService memberService) {
@@ -20,6 +30,7 @@ public class MemberController {
 
     @PostMapping("/members")
     public ResponseEntity createMember(@RequestBody MemberRequest request) {
+        logger.info("createMember request {}", request);
         MemberResponse member = memberService.createMember(request);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
     }
@@ -31,7 +42,8 @@ public class MemberController {
     }
 
     @PutMapping("/members/{id}")
-    public ResponseEntity<MemberResponse> updateMember(@PathVariable Long id, @RequestBody MemberRequest param) {
+    public ResponseEntity<MemberResponse> updateMember(@PathVariable Long id,
+        @RequestBody MemberRequest param) {
         memberService.updateMember(id, param);
         return ResponseEntity.ok().build();
     }
@@ -43,19 +55,22 @@ public class MemberController {
     }
 
     @GetMapping("/members/me")
-    public ResponseEntity<MemberResponse> findMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
+    public ResponseEntity<MemberResponse> findMemberOfMine(
+        @AuthenticationPrincipal LoginMember loginMember) {
         MemberResponse member = memberService.findMember(loginMember.getId());
         return ResponseEntity.ok().body(member);
     }
 
     @PutMapping("/members/me")
-    public ResponseEntity<MemberResponse> updateMemberOfMine(@AuthenticationPrincipal LoginMember loginMember, @RequestBody MemberRequest param) {
+    public ResponseEntity<MemberResponse> updateMemberOfMine(
+        @AuthenticationPrincipal LoginMember loginMember, @RequestBody MemberRequest param) {
         memberService.updateMember(loginMember.getId(), param);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/members/me")
-    public ResponseEntity<MemberResponse> deleteMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
+    public ResponseEntity<MemberResponse> deleteMemberOfMine(
+        @AuthenticationPrincipal LoginMember loginMember) {
         memberService.deleteMember(loginMember.getId());
         return ResponseEntity.noContent().build();
     }
