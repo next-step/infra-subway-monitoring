4117e373a6d5c84a2cc29a2526474aeb0615a1b5
Junhong-Kim <jhk1005jhk@gmail.com>
2021-12-18 13:22:04 +0900

    test: Stress 테스트 스크립트 및 결과 수정

--- k6/main/stress.js
+++ k6/main/stress.js
@@ -3,15 +3,15 @@ import {check, sleep} from 'k6';
 
 export let options = {
     stages: [
+        {duration: '5s', target: 10},
+        {duration: '5s', target: 20},
+        {duration: '5s', target: 30},
         {duration: '5s', target: 50},
         {duration: '5s', target: 100},
-        {duration: '10s', target: 100},
-        {duration: '5s', target: 150},
-        {duration: '5s', target: 150},
-        {duration: '10s', target: 200},
-        {duration: '5s', target: 200},
-        {duration: '5s', target: 100},
-        {duration: '10s', target: 0},
+        {duration: '5s', target: 50},
+        {duration: '5s', target: 20},
+        {duration: '5s', target: 10},
+        {duration: '5s', target: 0},
     ],
     thresholds: {
         http_req_duration: ['p(99)<200'],
--- k6/update_member/stress.js
+++ k6/update_member/stress.js
@@ -5,13 +5,13 @@ export let options = {
     stages: [
         {duration: '5s', target: 50},
         {duration: '5s', target: 100},
-        {duration: '10s', target: 100},
         {duration: '5s', target: 150},
+        {duration: '5s', target: 250},
+        {duration: '5s', target: 300},
+        {duration: '5s', target: 250},
         {duration: '5s', target: 150},
-        {duration: '10s', target: 200},
-        {duration: '5s', target: 200},
-        {duration: '5s', target: 100},
-        {duration: '10s', target: 0},
+        {duration: '5s', target: 50},
+        {duration: '5s', target: 0},
     ],
     thresholds: {
         http_req_duration: ['p(99)<200'],
