62142490556a37e818f885aea679625f8e1f4844
Seongmin Park <smpark911020@gmail.com>
2021-12-19 08:36:19 +0900

    1단계 - 로깅과 모니터링 (#178)
    * feat: Application Log 파일로 저장
    
    * refactor: properties file 수정
    
    * refactor: logback 수정
    
    * feat: 1단계 미션 수행
    
    * refactor: test application.properties 수정
    
    * refactor: Application log 경로 수정

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -1,5 +1,7 @@
 package nextstep.subway.map.ui;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
@@ -7,16 +9,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static nextstep.subway.common.LogUtils.fileLog;
+
 @RestController
 public class MapController {
-    private MapService mapService;
 
-    public MapController(MapService mapService) {
+    private final MapService mapService;
+    private final ObjectMapper objectMapper;
+
+    public MapController(MapService mapService, ObjectMapper objectMapper) {
         this.mapService = mapService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/paths")
-    public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
-        return ResponseEntity.ok(mapService.findPath(source, target));
+    public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) throws JsonProcessingException {
+        fileLog.info("SourceStationId: {} / TargetStationId: {}", source, target);
+        PathResponse pathResponse = mapService.findPath(source, target);
+        fileLog.info("PathResponse: {}", objectMapper.writeValueAsString(pathResponse));
+        return ResponseEntity.ok(pathResponse);
     }
 }
