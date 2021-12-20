307a45473648a2509c7392bcf89e19cb38713d6a
Ubuntu <ddabizz@gmail.com>
2021-12-20 11:36:31 +0000

    k6 script result

--- k6/smoke_manyDatas.js
+++ k6/smoke_manyDatas.js
@@ -15,5 +15,5 @@ const BASE_URL = 'https://all-forone.p-e.kr/stations';
 export default function ()  {
     let pageResponse = http.get(BASE_URL);
     check(pageResponse, { 'page loading complete': (response) => response.status === 200 });
-    sleep(1);
+	sleep(1);
 };
