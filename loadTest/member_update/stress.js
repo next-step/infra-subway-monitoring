399216f3a5080ba44766444ef974ed643da056ad
lewisseo91 <togokr@naver.com>
2021-12-22 23:20:36 +0900

    fix(test): update stress 결과 좀 더 곡선으로 증가하도록 변경
    - update stress 결과 좀 더 곡선으로 증가하도록 변경

--- loadTest/member_update/stress.js
+++ loadTest/member_update/stress.js
@@ -4,8 +4,10 @@ import { check, group, sleep, fail } from 'k6';
 export let options = {
   stages: [
     { duration: '2s', target: 300 }, // below normal load
-    { duration: '50s', target: 1000 },
-    { duration: '10s', target: 0 }, // scale down. Recovery stage.
+    { duration: '10s', target: 600 },
+    { duration: '10s', target: 700 },
+    { duration: '10s', target: 1000 },
+    { duration: '3s', target: 0 }, // scale down. Recovery stage.
   ],
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
