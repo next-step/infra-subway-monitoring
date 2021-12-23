8a37d97e4bab5ce0d0143ccfd4d4f82559ab25ca
koola97620 <koola97620@nate.com>
2021-12-24 02:37:13 +0900

    docs: 부하테스트 스크립트 작성

---
+++ loadtest/main/load.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 300 },
+        { duration: '2m', target: 500 },
+        { duration: '2m', target: 600 },
+        { duration: '10s', target: 0 },
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'http://15.164.72.205:8080/';
+
+export default function ()  {
+
+    http.get(`${BASE_URL}`);
+
+    sleep(1);
+};
---
+++ loadtest/main/smoke.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    vus: 1, // 1 user looping for 1 minute
+    duration: '10s',
+
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'http://15.164.72.205:8080/';
+
+export default function ()  {
+
+    http.get(`${BASE_URL}`);
+
+    sleep(1);
+};
