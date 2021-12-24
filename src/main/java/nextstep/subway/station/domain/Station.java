68a972a397392e930842d91fab21c785e4871cdc
박창혁 <changchang73@pionnet.co.kr>
2021-12-24 21:41:49 +0900

    fix: 테스트 오류 수정

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -1,10 +1,15 @@
 package nextstep.subway.station.domain;
 
-import nextstep.subway.common.BaseEntity;
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import nextstep.subway.common.BaseEntity;
 
 @Entity
 public class Station extends BaseEntity implements Serializable {
