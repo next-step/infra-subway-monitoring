b489f96ff3eb8952e6395685721aff9c5bd1fc85
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-12-30 01:19:44 +0900

    refactor: 라인 조회 쿼리에서 sleep(3) 제거

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -1,12 +1,11 @@
 package nextstep.subway.line.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface LineRepository extends JpaRepository<Line, Long> {
 
-    @Query(value = "SELECT * FROM line WHERE SLEEP(3)", nativeQuery = true)
+    @Query(value = "SELECT * FROM line", nativeQuery = true)
     List<Line> findAll();
 }
