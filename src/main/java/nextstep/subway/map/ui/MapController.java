3de477e07d1604d36d64464bbf20f4d96cdb50c7
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-12-30 00:14:50 +0900

    feat: 컨트롤러 로깅 설정

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -1,5 +1,6 @@
 package nextstep.subway.map.ui;
 
+import nextstep.subway.common.JsonLogging;
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
-    private MapService mapService;
 
-    public MapController(MapService mapService) {
+    private final MapService mapService;
+
+    public MapController(final MapService mapService) {
         this.mapService = mapService;
     }
 
     @GetMapping("/paths")
-    public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
+    @JsonLogging
+    public ResponseEntity<PathResponse> findPath(
+        @RequestParam final Long source,
+        @RequestParam final Long target
+    ) {
         return ResponseEntity.ok(mapService.findPath(source, target));
     }
 }
