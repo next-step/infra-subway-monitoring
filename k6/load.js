32c3c30d35114f9a9b83ceed95dd6642587f475a
seul <eeseul.p@gmail.com>
2022-02-17 17:50:43 +0900

    feat: 1단계 미션 제출

---
+++ k6/load.js
@@ -0,0 +1,49 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '10m', target: 23 },
+        { duration: '20m', target: 23 },
+        { duration: '10m', target: 0 },
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'https://parkeeseul.kro.kr';
+const USERNAME = 'test@test.com';
+const PASSWORD = 'test';
+
+export default function ()  {
+
+    var payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
+    var params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${loginRes.json('accessToken')}`,
+        },
+    };
+    let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+    check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+    sleep(1);
+};
