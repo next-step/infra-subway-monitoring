2961b9310751f4d13deb07b7b49bc306e0916192
JaeHan-Kim <hgal8877@gmail.com>
2022-02-19 20:49:05 +0900

    docs: 부하분산 스크립트 작성

---
+++ k6/load.js
@@ -0,0 +1,48 @@
+import http from 'k6/http';
+import { check, group, sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '10m', target: 3 },
+        { duration: '30m', target: 3 },
+        { duration: '10m', target: 0 },
+    ],
+    thresholds: {
+        'http_req_duration': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'https://kimjaehan.kro.kr/';
+const USERNAME = 'test@test.com';
+const PASSWORD = 'test1';
+
+export default () => {
+    var payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
+    var params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${loginRes.json('accessToken')}`,
+        },
+    };
+    let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+    check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+    sleep(1);
+};
