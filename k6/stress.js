8b82de83107ebbd8f170739e0aef9413c4ccc18c
이지영 <jiyounglee@gloud.io>
2022-03-26 15:32:51 +0900

    chore: load, smoke, stress

---
+++ k6/stress.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '5m', target: 100 },
+        { duration: '5m', target: 200 },
+        { duration: '5m', target: 300 },
+        { duration: '10m', target: 400 },
+        { duration: '10m', target: 500 },
+        { duration: '10m', target: 600 },
+        { duration: '10m', target: 0 }
+    ],
+};
+
+
+const BASE_URL = 'https://tuliplee.o-r.kr';
+export default () => {
+    const res = http.get(`${BASE_URL}/path`);
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
+
+    const object= http.get(`${BASE_URL}/paths/?source=1&target=8`).json();
+    check(object, { 'path': (obj) => obj.distance === 15 });
+
+    sleep(1);
+};
