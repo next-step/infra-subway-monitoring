b62f626a305b2687ee00fd4f0ec9a730d329d4e9
jiyounglee-gloud <95066911+jiyounglee-gloud@users.noreply.github.com>
2022-03-29 09:45:15 +0900

    docs: 성능테스트 개선2

--- k6/load.js
+++ k6/load.js
@@ -3,8 +3,8 @@ import { check, group, sleep, fail } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '5m', target: 4 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-        { duration: '10m', target: 10 }, // stay at 100 users for 10 minutes
+        { duration: '5m', target: 28 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '10m', target: 86 }, // stay at 100 users for 10 minutes
         { duration: '5m', target: 0 }, // ramp-down to 0 users
     ],
     thresholds: {
