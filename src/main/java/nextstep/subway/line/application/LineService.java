1201512056245e25ffccf2eb6a963a8bb0977438
ka0oll <jy.jeong8802@gmail.com>
2022-04-01 01:12:58 +0900

    feat : line api 캐시 작업

--- src/main/java/nextstep/subway/line/application/LineService.java
+++ src/main/java/nextstep/subway/line/application/LineService.java
@@ -7,6 +7,10 @@ import nextstep.subway.line.dto.LineResponse;
 import nextstep.subway.line.dto.SectionRequest;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +35,7 @@ public class LineService {
         return LineResponse.of(persistLine);
     }
 
+    @Cacheable(cacheNames = "line" ,value = "line", key = "'all'")
     public List<LineResponse> findLineResponses() {
         List<Line> persistLines = lineRepository.findAll();
         return persistLines.stream()
@@ -42,21 +47,31 @@ public class LineService {
         return lineRepository.findAll();
     }
 
+
     public Line findLineById(Long id) {
         return lineRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 
 
+    @Cacheable(cacheNames = "line" ,value = "line", key = "#id")
     public LineResponse findLineResponseById(Long id) {
         Line persistLine = findLineById(id);
         return LineResponse.of(persistLine);
     }
 
+    @CachePut(value = "line", key = "#id")
+    @CacheEvict(value = "line", key = "all")
     public void updateLine(Long id, LineRequest lineUpdateRequest) {
         Line persistLine = lineRepository.findById(id).orElseThrow(RuntimeException::new);
         persistLine.update(new Line(lineUpdateRequest.getName(), lineUpdateRequest.getColor()));
     }
 
+    @Caching(
+        evict = {
+                @CacheEvict(value = "line", key = "all"),
+                @CacheEvict(value = "line", key = "#id")
+        }
+    )
     public void deleteLineById(Long id) {
         lineRepository.deleteById(id);
     }
