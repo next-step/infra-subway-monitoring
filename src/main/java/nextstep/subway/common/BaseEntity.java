8529ddd3df0374a9a18e0a3fe22e5a7339abc5eb
Gyeongseo Woo <ugs1575@gmail.com>
2022-04-02 18:35:57 +0900

    feature: line redis 적용

--- src/main/java/nextstep/subway/common/BaseEntity.java
+++ src/main/java/nextstep/subway/common/BaseEntity.java
@@ -1,5 +1,9 @@
 package nextstep.subway.common;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -17,10 +21,14 @@ public class BaseEntity {
     @LastModifiedDate
     private LocalDateTime modifiedDate;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     public LocalDateTime getCreatedDate() {
         return createdDate;
     }
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     public LocalDateTime getModifiedDate() {
         return modifiedDate;
     }
