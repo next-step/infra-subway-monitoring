1acbd7e760af04ac1239751d31ed257497703c53
Hyoun Keun <hyounkeun2009@gmail.com>
2022-04-09 11:30:50 +0900

    test: Modify load.js VM condition.

--- k6/script/load.js
+++ k6/script/load.js
@@ -4,11 +4,6 @@ import { check, group, sleep, fail } from 'k6';
 export let options = {
     stages: [
         {duration: "10s", target: 100},
-        {duration: "10s", target: 100},
-        {duration: "10s", target: 100},
-        {duration: "10s", target: 100},
-        {duration: "10s", target: 100},
-        {duration: "10s", target: 100},
     ],
     thresholds: {
         http_req_duration: ["p(99)<100"], // 99% of requests must complete below 0.1s
