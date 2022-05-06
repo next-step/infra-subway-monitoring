c50dee3ab0a92bb21715c1d7d3a8683c1d1fcf1c
dibtp1221 <dibtp1221@gmail.com>
2022-05-07 04:19:24 +0900

    로깅

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,13 +3,20 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 @RestController
 public class AuthController {
+
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
+
     private AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -18,6 +25,9 @@ public class AuthController {
 
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
+        jsonLogger.info("{}",
+                kv("login_email", request.getEmail())
+        );
         TokenResponse token = authService.login(request);
         return ResponseEntity.ok().body(token);
     }
--- src/main/java/nextstep/subway/member/ui/MemberController.java
+++ src/main/java/nextstep/subway/member/ui/MemberController.java
@@ -5,6 +5,8 @@ import nextstep.subway.auth.domain.LoginMember;
 import nextstep.subway.member.application.MemberService;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,10 @@ import java.net.URI;
 
 @RestController
 public class MemberController {
+
+    private static final Logger log = LoggerFactory.getLogger(MemberController.class);
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+
     private MemberService memberService;
 
     public MemberController(MemberService memberService) {
@@ -20,6 +26,9 @@ public class MemberController {
 
     @PostMapping("/members")
     public ResponseEntity createMember(@RequestBody MemberRequest request) {
+        log.info("가입");
+        fileLogger.info("가입 {}", request.getEmail());
+        
         MemberResponse member = memberService.createMember(request);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
     }
