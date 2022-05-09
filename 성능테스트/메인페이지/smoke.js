14fb160eee5fbb393e93bd275bc36ca3c6b71f8d
jiwonKwon <jiwonkwon419@gmail.com>
2022-05-10 00:17:45 +0900

    feat : 성능테스트 (메인페이지)

---
+++ 성능테스트/메인페이지/smoke.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    vus: 1, // 1 user looping for 1 minute
+    duration: '10s',
+
+    thresholds: {
+        http_req_duration: ['p(99)<150'], // 99% of requests must complete below 0.15s
+    },
+};
+
+const BASE_URL = 'https://jiwonkwon-infra.p-e.kr/';
+
+export default function ()  {
+    http.get(`${BASE_URL}`);
+    sleep(1);
+};
