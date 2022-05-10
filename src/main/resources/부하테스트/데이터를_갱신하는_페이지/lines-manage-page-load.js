e24f13b8df5b03081a4a24fbf9c25f8cebb4fa2f
devrunner21 <devrunner21@gmail.com>
2022-05-10 22:53:45 +0900

    refactor: 데이터 갱신 페이지 테스트에 로그인 로직을 추가하라

--- src/main/resources/부하테스트/데이터를_갱신하는_페이지/lines-manage-page-load.js
+++ src/main/resources/부하테스트/데이터를_갱신하는_페이지/lines-manage-page-load.js
@@ -5,6 +5,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 const BASE_URL = 'https://devrunner21.kro.kr/';
+const USERNAME = 'devrunner21@gmail.com';
+const PASSWORD = '1234';
 const AVG_VUSER = 2;
 const MAX_VUSER = 17;
 
@@ -24,27 +26,53 @@ export let options = {
 
 export default function () {
 
+    // 로그인 요청
+    var loginPayload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
     var params = {
         headers: {
             'Content-Type': 'application/json',
         },
     };
 
+    let loginRes = http.post(`${BASE_URL}/login/token`, loginPayload, params);
+
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+    let authHeaders = {
+        headers: {
+            'Authorization': `Bearer ${loginRes.json('accessToken')}`,
+            'Content-Type': 'application/json'
+        },
+    };
+
     // 노선 목록 조회
-    let getLinesBeforeRes = http.get(`${BASE_URL}/lines`, params);
+    let getLinesBeforeRes = http.get(`${BASE_URL}/lines`, authHeaders);
     check(getLinesBeforeRes, {
         'get lines request successful': (resp) => resp.status == 200,
     });
 
     // 노선 수정
-    const payload = {id : 1, name : '1호선', color : 'red lighten-1'};
-    let updateLinesRes = http.put(`${BASE_URL}/lines/1`, params, payload);
+    const payload = {
+        name: '1호선',
+        color: 'red lighten-1',
+        upStationId: null,
+        downStationId: null,
+        distance: 0
+    };
+
+    let updateLinesRes = http.put(`${BASE_URL}/lines/1`, JSON.stringify(payload), authHeaders);
     check(updateLinesRes, {
         'update lines is successful': (resp) => resp.status == 200,
     });
 
     // 노선 목록 조회
-    let getLinesAfterRes = http.get(`${BASE_URL}/lines`, params);
+    let getLinesAfterRes = http.get(`${BASE_URL}/lines`, authHeaders);
     check(getLinesAfterRes, {
         'get lines request successful': (resp) => resp.status == 200,
     });
