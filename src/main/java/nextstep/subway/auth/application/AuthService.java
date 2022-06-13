d3964d1fcdb7e27d0a7091aead548b06bb75d6a5
heowc <heowc1992@gmail.com>
2022-06-13 13:56:24 +0900

    feat(로그인): log 추가

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,12 +6,17 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
+
+    private static final Logger logger = LoggerFactory.getLogger("file");
+
     private MemberRepository memberRepository;
     private JwtTokenProvider jwtTokenProvider;
 
@@ -25,16 +30,20 @@ public class AuthService {
         member.checkPassword(request.getPassword());
 
         String token = jwtTokenProvider.createToken(request.getEmail());
+
+        logger.info("logged in successfully. member={}", member);
         return new TokenResponse(token);
     }
 
     public LoginMember findMemberByToken(String credentials) {
         if (!jwtTokenProvider.validateToken(credentials)) {
+            logger.warn("token is invalid. token={}", credentials);
             throw new AuthorizationException();
         }
 
         String email = jwtTokenProvider.getPayload(credentials);
         Member member = memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
+        logger.info("validated token in successfully. member={}", member);
         return new LoginMember(member.getId(), member.getEmail(), member.getAge());
     }
 }
--- src/main/java/nextstep/subway/member/domain/Member.java
+++ src/main/java/nextstep/subway/member/domain/Member.java
@@ -54,4 +54,14 @@ public class Member extends BaseEntity {
             throw new AuthorizationException();
         }
     }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", email=[MASKED]" +
+                ", password=[MASKED]" +
+                ", age=" + age +
+                '}';
+    }
 }
