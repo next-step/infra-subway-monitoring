64a976ece0f0929e2d769807c3b1ea3a25d62934
jhsong2580 <thdwmdgns@naver.com>
2022-06-17 17:43:57 +0900

    test: profile 설정

--- src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/acceptance/LineAcceptanceTest.java
@@ -22,6 +22,7 @@ import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철 노선 관련 기능")
+@ActiveProfiles("test")
 public class LineAcceptanceTest extends AcceptanceTest {
     private StationResponse 강남역;
     private StationResponse 광교역;
