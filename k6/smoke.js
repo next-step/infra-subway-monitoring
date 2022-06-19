fc3b93cf7b201aad481486edd0b1f78fa139ad98
Donghee Han <handh0413@gmail.com>
2022-06-19 22:26:58 +0900

    doc(smoke.js): 내용 추가
    - 회원가입 페이지 이동
    - 로그인 수행
    - 로그인 성공 유무 확인
    - 로그인 실패 시 계정 생성

--- k6/smoke.js
+++ k6/smoke.js
@@ -13,16 +13,14 @@ export let options = {
 const BASE_URL = 'https://handh.kro.kr';
 const USERNAME = 'nextstep@nextstep.camp';
 const PASSWORD = 'password';
+const AGE = 20;
 
 export default function ()  {
-
-    goToMainPage();
-
-    goToLoginPage();
 
     var payload = JSON.stringify({
         email: USERNAME,
         password: PASSWORD,
+        age: AGE,
     });
 
     var params = {
@@ -31,21 +29,33 @@ export default function ()  {
         },
     };
 
+    goToMainPage();
 
-    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+    goToLoginPage();
 
-    check(loginRes, {
-        'logged in successfully': (resp) => resp.json('accessToken') !== '',
-    });
+    goToJoinPage();
 
+    let loginRes = login(payload, params);
+
+    if (!isLoginSuccess(loginRes)) {
+        createMember(payload);
+        loginRes = login(payload, params);
+    }
 
     let authHeaders = {
         headers: {
             Authorization: `Bearer ${loginRes.json('accessToken')}`,
         },
     };
+
     let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
-    check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+    check(myObjects, {
+        'Unauthorized check ': (r) => r.status === 401,
+        'retrieved member': (obj) => obj.id != 0
+    });
+
+    // createMember(payload);
+
     sleep(1);
 };
 
@@ -62,5 +72,38 @@ function goToLoginPage() {
 
     const checkRes = check(res, {
         'goToLoginPage() status is 200': (r) => r.status === 200
+    });
+}
+
+function goToJoinPage() {
+    const res = http.get(`${BASE_URL}/join`);
+
+    const checkRes = check(res, {
+        'goToJoinPage() status is 200': (r) => r.status === 200
+    });
+}
+
+function login(payload, params) {
+    const loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+    check(loginRes, {
+        'login() ': (r) => r.status === 200,
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+    return loginRes;
+}
+
+function isLoginSuccess(response) {
+    return response.status === 200;
+}
+
+function createMember(payload) {
+    const res = http.post(`${BASE_URL}/members`, payload, {
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    const checkRes = check(res, {
+        'createMember() status is 201': (r) => r.status === 201,
     });
 }
