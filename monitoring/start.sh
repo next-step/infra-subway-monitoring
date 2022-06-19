9c93063d8b5160b47b6028b2ec69959c66f0cead
Sung-jin <osj4872@gmail.com>
2022-06-19 17:57:29 +0900

    chore: 모니터링 결과 stdout 파일로 저장할 수 있도록 스크립트 수정

--- monitoring/start.sh
+++ monitoring/start.sh
@@ -2,6 +2,10 @@
 
 SCRIPT_PATH=$(dirname "$0")
 
-for MONITORING_SCRIPT_FILE in $(find "$SCRIPT_PATH" -type f -name '*.js'); do
-  k6 run --out influxdb=http://localhost:8086/myk6db "$MONITORING_SCRIPT_FILE"
+for MONITORING_SCRIPT_FILE in $(find "$SCRIPT_PATH" -path ./monitoring/executor -prune -o -name '*.js' -print); do
+  NAME="$(basename "$MONITORING_SCRIPT_FILE" .js)"
+  OUTPUT_PATH=$(dirname "$MONITORING_SCRIPT_FILE")
+
+  mkdir -p "$OUTPUT_PATH/output"
+  k6 run --out influxdb=http://localhost:8086/myk6db "$MONITORING_SCRIPT_FILE" > "$OUTPUT_PATH/output/$NAME.log"
 done
