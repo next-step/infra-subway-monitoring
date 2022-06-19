1d5869ac147cf20a15859c112d7d613e12232ba9
otakjong <tj3828@naver.com>
2022-06-20 02:34:24 +0900

    feat: log 기능 추가

--- src/main/java/nextstep/subway/station/domain/Station.java
+++ src/main/java/nextstep/subway/station/domain/Station.java
@@ -1,13 +1,17 @@
 package nextstep.subway.station.domain;
 
-import nextstep.subway.common.BaseEntity;
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import nextstep.subway.common.BaseEntity;
 
 @Entity
 public class Station extends BaseEntity implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,15 +35,28 @@ public class Station extends BaseEntity implements Serializable {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Station station = (Station) o;
         return Objects.equals(id, station.id) &&
-                Objects.equals(name, station.name);
+            Objects.equals(name, station.name);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, name);
     }
+
+    @Override
+    public String toString() {
+        return "Station{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            '}';
+    }
+
 }
