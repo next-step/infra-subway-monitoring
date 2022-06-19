fcc0ec335aaa038f1ed2077fb34ed4e94d9b7162
권용일 <ksgkms23@likelion.org>
2022-06-19 22:42:11 +0900

    Step2 부하테스트 PR 요청드립니다! (#325)
    * docs(monitoring) docs 요구사항 정리
    
    * docs(monitoring) 부하테스트 전제조건 및 테스트 스크립트, 결과 공유
    
    * docs(monitoring) 부하테스트 전제조건 추가

---
+++ docs/stress/stress.js
@@ -0,0 +1,98 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 12 },
+        { duration: '1m', target: 24 },
+        { duration: '1m', target: 48 },
+        { duration: '1m', target: 96 },
+        { duration: '1m', target: 192 },
+        { duration: '4m', target: 192 },
+        { duration: '1m', target: 288 },
+        { duration: '4m', target: 288 },
+        { duration: '1m', target: 192 },
+        { duration: '1m', target: 96 },
+        { duration: '1m', target: 48},
+        { duration: '1m', target: 24},
+        { duration: '1m', target: 12},
+        { duration: '4m', target: 0},
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<100']
+    },
+};
+
+
+const BASE_URL = 'https://kwonyongil-infra.kro.kr/';
+const USERNAME = 'test@test';
+const PASSWORD = 'password';
+
+export default function ()  {
+
+    mainPage();
+
+    loginPage();
+
+    var accessToken = login(USERNAME, PASSWORD);
+
+    pathPage();
+
+    findFavorites(accessToken);
+
+    findPath();
+
+    sleep(1);
+};
+
+function mainPage() {
+    var response = http.get(`${BASE_URL}/`);
+    check(response, {'main page' : (res) => res.status === 200 });
+}
+
+function loginPage() {
+    var response = http.get(`${BASE_URL}/login`);
+    check(response, {'login page' : (res) => res.status === 200 });
+}
+
+function pathPage() {
+    var response = http.get(`${BASE_URL}/path`);
+    check(response, {'path page' : (res) => res.status === 200 });
+}
+
+function login(email, password) {
+    var payload = JSON.stringify({
+        email: email,
+        password: password,
+    });
+
+    var params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+    check(loginRes, {
+        'logged in successfully': (res) => res.json('accessToken') !== '',
+    });
+
+    return loginRes.json('accessToken');
+}
+
+function findFavorites(accessToken) {
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+     };
+     var response = http.get(`${BASE_URL}/favorites`, authHeaders);
+     check(response, { 'find favorites': (res) => res.status === 200});
+}
+
+function findPath() {
+    var response = http.get(`${BASE_URL}/paths?source=1&target=2`);
+
+    check(response, {'find path': (resp) => resp.status === 200 });
+}
