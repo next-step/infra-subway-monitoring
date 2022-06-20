f49f1517688387f3289fe8d901114c99e9566191
jhsong2580 <thdwmdgns@naver.com>
2022-06-21 00:43:34 +0900

    Step2 리뷰 요청 드립니다.  (#342)
    * docs: step2 부하테스트 전제조건 설정
    
    * docs: step2 부하테스트 스크립트 및 결과 등록

---
+++ readmeSource/loadTest/k6Test/script/smoke.js
@@ -0,0 +1,101 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+  vus: 2, // 1 user looping for 1 minute
+  duration: '40m',
+
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const BASE_URL = 'https://jhsong2580.kro.kr';
+const USERNAME = 'thdwmdgns@naver.com';
+const PASSWORD = 'Tacgu6-tonhiv-baqdes';
+
+
+export default function ()  {
+  accessIndex();
+
+  let token = login(USERNAME, PASSWORD);
+
+  getPath(token, 1, 10);
+
+  sleep(1);
+};
+
+
+
+
+
+function login(id, pw)  {
+
+  var payload = JSON.stringify({
+    email: id,
+    password: pw,
+  });
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+
+  let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+  check(loginRes, {
+    'logged in successfully': (resp) => resp.json('accessToken') !== '',
+  });
+
+  let token = loginRes.json('accessToken');
+  let authHeaders = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+  check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+
+  return token;
+};
+
+
+
+function getPath(token, source, target)  {
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+  };
+
+
+  let pathRes = http.get(`${BASE_URL}/path?source=${source}&target=${target}`, params);
+
+  check(pathRes, {
+    'getData Success': (resp) => resp.status === 200,
+  });
+
+};
+
+
+
+function accessIndex()  {
+
+  var params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+
+  let pathRes = http.get(`${BASE_URL}`, params);
+
+  check(pathRes, {
+    'indexPageAccess': (resp) => resp.status === 200,
+  });
+
+};
