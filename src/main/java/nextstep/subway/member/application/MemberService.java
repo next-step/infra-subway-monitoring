a60920ac2801d2d40b27f07496e772017d9e638b
tyakamyz <tyakamyz@naver.com>
2022-06-20 16:13:45 +0900

    feat: 회원가입, 로그인 등의 이벤트에 로깅 설정

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,13 +4,17 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
-    private MemberRepository memberRepository;
+
+    private static final Logger LOG = LoggerFactory.getLogger("file");
+    private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -18,6 +22,7 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
+        LOG.info("createMember success. memberId: {}", member.getId());
         return MemberResponse.of(member);
     }
 
@@ -29,9 +34,11 @@ public class MemberService {
     public void updateMember(Long id, MemberRequest param) {
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
         member.update(param.toMember());
+        LOG.info("updateMember success. memberId: {}", member.getId());
     }
 
     public void deleteMember(Long id) {
         memberRepository.deleteById(id);
+        LOG.info("deleteMember success. memberId: {}", id);
     }
 }
