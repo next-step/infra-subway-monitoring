5e916cdf4ed5b4326f9b9b98a1e141011e33e361
soob-forest <dltntnqo@naver.com>
2022-06-23 01:22:06 +0900

    feat: 경로 검색 이벤트 로깅

--- src/main/java/nextstep/subway/map/application/PathService.java
+++ src/main/java/nextstep/subway/map/application/PathService.java
@@ -1,5 +1,7 @@
 package nextstep.subway.map.application;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.map.domain.SectionEdge;
 import nextstep.subway.map.domain.SubwayGraph;
@@ -7,6 +9,8 @@ import nextstep.subway.map.domain.SubwayPath;
 import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +18,11 @@ import java.util.stream.Collectors;
 
 @Service
 public class PathService {
+    private static final Logger logger = LoggerFactory.getLogger(PathService.class);
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
+
     public SubwayPath findPath(List<Line> lines, Station source, Station target) {
+
         SubwayGraph graph = new SubwayGraph(SectionEdge.class);
         graph.addVertexWith(lines);
         graph.addEdge(lines);
@@ -23,7 +31,21 @@ public class PathService {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         GraphPath<Station, SectionEdge> path = dijkstraShortestPath.getPath(source, target);
 
-        return convertSubwayPath(path);
+        SubwayPath subwayPath = convertSubwayPath(path);
+        findPathLogging(source, target, subwayPath);
+
+        return subwayPath;
+    }
+
+    private void findPathLogging(Station source, Station target, SubwayPath subwayPath) {
+        logger.info("find shortest path from {} to {}, stations : {}",
+                source.getName(),
+                target.getName(),
+                subwayPath.getStations());
+        jsonLogger.info("find shortest path from {} to {}, {}",
+                kv("Source", source.getName()),
+                kv("Target", target.getName()),
+                kv("Stations", subwayPath.getStations()));
     }
 
     private SubwayPath convertSubwayPath(GraphPath graphPath) {
