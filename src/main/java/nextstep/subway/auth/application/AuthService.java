f2a46e8bd160aad67833911d12f9a1a15683d2a1
june <kimjy4536@hotmail.com>
2022-06-22 10:57:37 +0900

    Step3 - 로깅, 모니터링  (#355)
    * feat: gradle implementation 추가
    
    * feat: logback 설정
    
    * feat: 회원가입, 로그인, 경로찾기 이벤트 로깅 설정;
    
    * feat: add aws configure
    
    * fix: query error
    
    * doc: 서버 로깅 경로 작성
    
    * feat: cloudwatch 셋업
    
    * fix: 목푯값 재설정
    
    * chore: remove log file

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,12 +6,17 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
+
+    private static final Logger LOG = LoggerFactory.getLogger("file");
+
     private MemberRepository memberRepository;
     private JwtTokenProvider jwtTokenProvider;
 
@@ -24,6 +29,7 @@ public class AuthService {
         Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(AuthorizationException::new);
         member.checkPassword(request.getPassword());
 
+        LOG.info("login - memberId:{}", member.getId());
         String token = jwtTokenProvider.createToken(request.getEmail());
         return new TokenResponse(token);
     }
