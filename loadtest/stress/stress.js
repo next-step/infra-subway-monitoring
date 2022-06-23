88f3be8f404833506fc61e47db48888b44b8d4bc
Ttungga <torb2002@gmail.com>
2022-06-24 04:32:01 +0900

    docs: step2 부하 테스트 스크립트 및 결과 이미지 추가

---
+++ loadtest/stress/stress.js
@@ -0,0 +1,83 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 100 },
+        { duration: '1m', target: 200 },
+        { duration: '1m', target: 300 },
+        { duration: '1m', target: 400 },
+        { duration: '1m', target: 400 },
+        { duration: '1m', target: 300 },
+        { duration: '1m', target: 200 },
+        { duration: '1m', target: 100 },
+        { duration: '1m', target: 40 },
+        { duration: '1m', target: 10 }
+    ],
+
+    thresholds: {
+        http_req_duration: ['p(99)<100'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'https://ttungga.r-e.kr/';
+const USERNAME = 'test@test';
+const PASSWORD = 'test';
+
+export default function ()  {
+    mainPage();
+
+    let accessToken = login(USERNAME, PASSWORD);
+
+    pathPage(accessToken);
+    getPath(accessToken);
+
+    sleep(1);
+};
+
+function mainPage() {
+    let response = http.get(BASE_URL);
+    check(response, {'메인 페이지 접속' : (res) => res.status === 200 });
+}
+
+function login(email, password) {
+    let payload = JSON.stringify({
+        email: email,
+        password: password,
+    });
+
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+    check(loginRes, {
+        '로그인': (res) => res.json('accessToken') !== '',
+    });
+
+    return loginRes.json('accessToken');
+}
+
+function pathPage(accessToken) {
+    let header = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    };
+    let response = http.get(`${BASE_URL}/path`, header);
+    check(response, {'경로 검색 페이지 접속' : (res) => res.status === 200 });
+}
+
+function getPath(accessToken) {
+    let header = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+        }
+    };
+    let response = http.get(`${BASE_URL}/path?source=1&target=2`, header);
+    check(response, {'경로 검색' : (res) => res.status === 200 });
+}
