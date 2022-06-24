59d931d9ed27b70ffdd52ef3c89da431ab53fc71
seungin yang <rhfpdk92@naver.com>
2022-06-25 00:20:14 +0900

    fix: 로그인 로그 추가

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -28,6 +28,7 @@ public class AuthService {
         member.checkPassword(request.getPassword());
 
         String token = jwtTokenProvider.createToken(request.getEmail());
+        logger.info("login success: {}", member.getId());
         return new TokenResponse(token);
     }
 
@@ -38,7 +39,7 @@ public class AuthService {
 
         String email = jwtTokenProvider.getPayload(credentials);
         Member member = memberRepository.findByEmail(email).orElseThrow(RuntimeException::new);
-        logger.info("memberId {} ,login success! ",member.getId());
+        logger.info("memberId {} ,login success! ", member.getId());
         return new LoginMember(member.getId(), member.getEmail(), member.getAge());
     }
 }
