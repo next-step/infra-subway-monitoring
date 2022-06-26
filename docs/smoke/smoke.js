6254a0f84821faacc0d250c20cc28eea353a0da6
piopoi <kangsw1988@gmail.com>
2022-06-26 09:54:24 +0900

    Step2 - 부하테스트 (#366)
    * docs(README.md): 1단계 피드백 반영
    
    * refactor(LineRepostory): query에서 sleep 제거
    
    * ci: 테스트를 위한 설정파일 및 배포스크립트 추가
    
    * docs(README.md): 2단계 답변 추가
    
    * docs: smoke/load/stress 테스트 스크립트 및 결과 스크린샷 추가
    
    * ci(smoke.js):  주석 수정
    
    * ci: 부하테스트 스크립트에서 sleep 제거
    
    * docs: 피드백 반영

---
+++ docs/smoke/smoke.js
@@ -0,0 +1,98 @@
+import http from 'k6/http';
+import {check} from 'k6';
+
+export let options = {
+    vus: 1,
+    duration: '10s',
+
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'https://piopoi.kro.kr';
+const USERNAME = 'test@email.com';
+const PASSWORD = 'password';
+
+export default function () {
+    //메인페이지 접속
+    accessMainPage();
+
+    //로그인
+    let token = login();
+
+    //회원 정보 조회
+    getMemberInfo(token);
+
+    //회원 정보 수정
+    updateMemberInfo(token);
+
+    //즐겨찾기 조회
+    getFavorites(token);
+}
+
+function accessMainPage() {
+    let response = http.get(`${BASE_URL}`);
+    check(response, {
+        'access main page successfully': (res) => res.status === 200
+    });
+}
+
+function login() {
+    let payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+    let params = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    let response = http.post(`${BASE_URL}/login/token`, payload, params);
+    check(response, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== ''
+    });
+    return response.json('accessToken');
+}
+
+function getMemberInfo(token) {
+    let params = {
+        headers: {
+            Authorization: `Bearer ` + token
+        }
+    };
+    let response = http.get(`${BASE_URL}/members/me`, params).json();
+    check(response, {'get member info successfully': (obj) => obj.id != 0});
+}
+
+function updateMemberInfo(token) {
+    let payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+        age: 30
+    });
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ` + token
+        }
+    }
+    let response = http.post(`${BASE_URL}/members/me`, payload, params);
+    check(response, {
+        'member info update successfully': (resp) => resp.json('accessToken') !== ''
+    });
+}
+
+function getFavorites(token) {
+    let params = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ` + token
+        }
+    }
+    let response = http.get(`${BASE_URL}/favorites`, params).json();
+    check(response, {
+        'get favorites successfully': (obj) => obj.id != 0
+    });
+}
+
