015ccace02de9e834282c0b217e4fc16a157aa21
geunhwanlee <56829658+geunhwanlee@users.noreply.github.com>
2022-06-26 10:06:17 +0900

    Step3 - 로깅, 모니터링 (#361)
    * fix: local 환경설정 변경
    
    * refactor: 운영 환경 서브모듈 등록
    
    * fix: 슬로우쿼리 제거
    
    * feat: 로그 설정
    
    * feat: 로컬환경에 메소드 실행 로그 추가
    
    * feat: 회원가입, 회원탈퇴, 로그인 이벤트에 파일 로깅 설정
    
    * feat: 경로찾기 시 json 로깅 설정
    
    * feat: Spring Actuator Metric 수집
    
    * feat: Spring Actuator Metric 수집
    
    * docs: 3단계 - 로깅, 모니터링
    
    * fix: 설정 변경
    
    * fix: 주석 제거
    
    * fix: eol 추가
    
    * fix: 7개의 로그만 유지하고 나머지는 삭제

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,12 +4,16 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
+    private static final Logger log = LoggerFactory.getLogger("file");
+
     private MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
@@ -18,6 +22,9 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
+
+        log.info("join member id = {}", member.getId());
+
         return MemberResponse.of(member);
     }
 
@@ -32,6 +39,8 @@ public class MemberService {
     }
 
     public void deleteMember(Long id) {
+        log.info("delete member id = {}", id);
+
         memberRepository.deleteById(id);
     }
 }
