fbcf46476ce416c6c216c2958b645a263b3513c5
Lisc <qqwer153@gmail.com>
2022-06-27 12:55:59 +0900

    refactor: 피드백 반영

--- k6/script/load.js
+++ k6/script/load.js
@@ -3,11 +3,11 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1m', target: 3 },
-    { duration: '2m', target: 8 },
-    { duration: '3m', target: 12 },
-    { duration: '2m', target: 8 },
-    { duration: '1m', target: 3 },
+    { duration: '1m', target: 5 },
+    { duration: '2m', target: 13 },
+    { duration: '3m', target: 22 },
+    { duration: '2m', target: 13 },
+    { duration: '1m', target: 5 },
   ],
   thresholds: {
     http_req_duration: ['p(99) < 1500'],
