bb524efde1edd25ecc7ba847faa842b3b7e67723
songsimo <chy.song89@gmail.com>
2022-06-28 04:34:10 +0900

    feat: Application Log 파일로 저장하기

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,12 +6,16 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
+    private static final Logger log = LoggerFactory.getLogger("console");
+
     private MemberRepository memberRepository;
     private JwtTokenProvider jwtTokenProvider;
 
@@ -25,6 +29,8 @@ public class AuthService {
         member.checkPassword(request.getPassword());
 
         String token = jwtTokenProvider.createToken(request.getEmail());
+
+        log.info("Login Success MemberEmail: {}", member.getEmail());
         return new TokenResponse(token);
     }
 
