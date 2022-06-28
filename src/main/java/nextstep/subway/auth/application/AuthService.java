f932f9e5ffdf9258bcebafcd8a615e9e172464e8
kmmin78 <31556184+kmmin78@users.noreply.github.com>
2022-06-29 00:03:56 +0900

    Step3 - 로깅, 모니터링 (#400)
    * config step3 배포환경 설정
    
    * config step3 동작하지 않는 쿼리 수정
    
    * feat step3 Application Log 파일로 저장하기, Nginx Access Log 설정하기
    
    * feat step3 라인 조회 오류 쿼리 제거
    
    * feat step3 Cloudwatch로 모니터링
    
    Co-authored-by: kms <kmmin78@gmail.com>

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,12 +6,15 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
+    private static final Logger logger = LoggerFactory.getLogger("console");
     private MemberRepository memberRepository;
     private JwtTokenProvider jwtTokenProvider;
 
@@ -25,6 +28,7 @@ public class AuthService {
         member.checkPassword(request.getPassword());
 
         String token = jwtTokenProvider.createToken(request.getEmail());
+        logger.info("login id : {}", member.getId());
         return new TokenResponse(token);
     }
 
