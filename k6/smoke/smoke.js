9d12bb947b989990967c332416d34879e67a60c8
sonypark <sonypark@woowahan.com>
2022-06-30 00:03:26 +0900

    feat: 부하테스트 시나리오 및 스크립트 작성

---
+++ k6/smoke/smoke.js
@@ -0,0 +1,82 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export const options = {
+	vus: 1, // 1 user looping for 1 minute
+	duration: '1m',
+
+	thresholds: {
+		http_req_duration: ['p(99) < 1500'], // 99% of requests must complete below 1.5s
+	},
+};
+
+const BASE_URL = 'https://sonypark.shop';
+const USERNAME = 'sonypark@gmail.com';
+const PASSWORD = 'a123b';
+
+export default function ()  {
+	mainPage();
+
+	const accessToken = login();
+
+	getFavorites(accessToken);
+
+	getPath(accessToken);
+
+	sleep(1);
+};
+
+function mainPage() {
+	const mainPageRes = http.get(`${BASE_URL}`);
+	check(mainPageRes, {
+		'goToMainPage success': (resp) => resp.status == 200,
+	});
+}
+
+function login() {
+	const payload = JSON.stringify({
+		email: USERNAME,
+		password: PASSWORD,
+	});
+
+	const params = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+
+	const loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+	check(loginRes, {
+		'logged in successfully': (resp) => resp.json('accessToken') !== '',
+	});
+
+	return loginRes.json('accessToken');
+}
+
+function getFavorites(accessToken) {
+	const params = {
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ` + accessToken
+		}
+	}
+
+	const response = http.get(`${BASE_URL}/favorites`, params).json();
+
+	check(response, {
+		'get favorites successfully': (obj) => obj.id != 0
+	});
+}
+
+function getPath(accessToken) {
+	const header = {
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${accessToken}`,
+		}
+	};
+	const response = http.get(`${BASE_URL}/path?source=1&target=2`, header);
+
+	check(response, {'경로 검색' : (res) => res.status === 200 });
+}
