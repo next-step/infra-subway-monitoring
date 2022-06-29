5e025f0389f719fdfb3b737c124526819224dc01
Giwan Kim <giwankim@gmail.com>
2022-06-29 09:16:31 +0900

    fix: lower RPS

--- scripts/landing-page/stress.js
+++ scripts/landing-page/stress.js
@@ -3,13 +3,11 @@ import { check, group, sleep, fail } from "k6";
 
 export let options = {
   stages: [
-    { duration: "5m", target: 400 },
-    { duration: "5m", target: 800 },
-    { duration: "5m", target: 1600 },
-    { duration: "5m", target: 3200 },
-    { duration: "5m", target: 6400 },
-    { duration: "5m", target: 12800 },
-    { duration: "20s", target: 0 },
+    { duration: "1m", target: 200 },
+    { duration: "1m", target: 1000 },
+    { duration: "1m", target: 2000 },
+    { duration: "1m", target: 5000 },
+    { duration: "10s", target: 0 },
   ],
   thresholds: {
     http_req_duration: ["p(99)<150"], // 99% of requests must complete below 0.15s
