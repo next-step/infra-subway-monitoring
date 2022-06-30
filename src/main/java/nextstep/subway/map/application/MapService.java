3e7d58a4ceb1bb46bf8b06079c14398efd7f693c
SANGIK KIM <sangik.kim@enel.com>
2022-07-01 01:52:10 +0900

    feat(log): add application logs

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -1,5 +1,6 @@
 package nextstep.subway.map.application;
 
+import java.util.List;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.map.domain.SubwayPath;
@@ -7,14 +8,16 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
 public class MapService {
+    private static final Logger log = LoggerFactory.getLogger(MapService.class);
+
     private LineService lineService;
     private StationService stationService;
     private PathService pathService;
@@ -31,6 +34,7 @@ public class MapService {
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
 
+        log.info("{}에서 {}로 경로찾기를 시도했습니다.", sourceStation, targetStation);
         return PathResponseAssembler.assemble(subwayPath);
     }
 }
