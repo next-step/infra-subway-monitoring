e3ca93599130d4b968f70fb4b6698c6624451276
DongGyuJang <cdgu1129@knou.ac.kr>
2022-07-01 15:28:00 +0900

    feat: 로그 설정

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -2,6 +2,8 @@ package nextstep.subway.map.ui;
 
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
+
     private MapService mapService;
 
     public MapController(MapService mapService) {
@@ -17,6 +21,11 @@ public class MapController {
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
-        return ResponseEntity.ok(mapService.findPath(source, target));
+        jsonLogger.info("[Find Path Try] Source & Target ID: {}, {}", source, target);
+        PathResponse pathResponse = mapService.findPath(source, target);
+        jsonLogger.info("[Find Path] Source & Target ID: {}, {}", source, target);
+        jsonLogger.info("[Find Path] Stations: {}, Distance: {}", pathResponse.getStations(), pathResponse.getDistance());
+
+        return ResponseEntity.ok(pathResponse);
     }
 }
