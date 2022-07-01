2bb5945a5ca99ee9ef229d9783de94f767979aef
DongGyuJang <56859781+EastStar1129@users.noreply.github.com>
2022-07-02 00:31:00 +0900

    3단계 - 로깅, 모니터링 (#425)
    * docs: 요구사항 정리 및 리뷰반영
    
    * build: log에 필요한 lib추가 및 xml설정
    
    * feat: 로그 설정
    
    * feat: json 설정 추가
    
    * docs: 문서 업데이트
    
    * bulid: Spring Actuator Metric 수집을 위한 설정 추가
    
    * docs: 문서 업데이트

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -1,7 +1,11 @@
 package nextstep.subway.map.ui;
 
+import static  net.logstash.logback.argument.StructuredArguments.kv;
+
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
+
     private MapService mapService;
 
     public MapController(MapService mapService) {
@@ -17,6 +23,11 @@ public class MapController {
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
-        return ResponseEntity.ok(mapService.findPath(source, target));
+        jsonLogger.info("[Find Path Try] Source & Target ID: {}, {}", kv("출발지", source), kv("도착지", target));
+        PathResponse pathResponse = mapService.findPath(source, target);
+        jsonLogger.info("[Find Path] Source & Target ID: {}, {}", kv("출발지", source), kv("도착지", target));
+        jsonLogger.info("[Find Path] Stations: {}, Distance: {}", kv("역들", pathResponse.getStations()), kv("거리", pathResponse.getDistance()));
+
+        return ResponseEntity.ok(pathResponse);
     }
 }
