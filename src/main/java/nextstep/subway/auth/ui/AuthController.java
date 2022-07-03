ac7c6696c09e53ee7f497c6cc449522008d01e69
tasklet1579 <tasklet1579@gmail.com>
2022-07-04 00:32:18 +0900

    feat : 로그인 로깅

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,6 +3,8 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    private final static Logger log = LoggerFactory.getLogger("file");
+
     private AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -18,7 +22,9 @@ public class AuthController {
 
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
+        log.info("로그인 요청: {}", request.toString());
         TokenResponse token = authService.login(request);
+        log.info("로그인 결과: {}", token.toString() != null ? "성공" : "실패");
         return ResponseEntity.ok().body(token);
     }
 }
