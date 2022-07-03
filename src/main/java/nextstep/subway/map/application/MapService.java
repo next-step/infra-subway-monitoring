409229222ae433a3ee05a7aa5a9ec8879fba8f3c
Giwan Kim <giwankim@gmail.com>
2022-07-03 19:07:13 +0900

    feat: add spring actuator metric

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -1,7 +1,5 @@
 package nextstep.subway.map.application;
 
-import static net.logstash.logback.argument.StructuredArguments.kv;
-
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.map.domain.SubwayPath;
@@ -9,8 +7,6 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +16,6 @@ import java.util.List;
 @Transactional
 public class MapService {
 
-  private static final Logger json = LoggerFactory.getLogger("json");
   private LineService lineService;
   private StationService stationService;
   private PathService pathService;
@@ -37,10 +32,6 @@ public class MapService {
     Station sourceStation = stationService.findById(source);
     Station targetStation = stationService.findById(target);
     SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
-
-    json.info("{}, {}",
-        kv("출발지", sourceStation.getName()),
-        kv("도착지", targetStation.getName()));
 
     return PathResponseAssembler.assemble(subwayPath);
   }
