aa6761ee8811d1de0620ff81bcd4e3935637aa2d
Yul Jeon <notkhaki@gmail.com>
2022-07-03 20:15:25 +0900

    Step3 - 로깅, 모니터링 (#439)
    * fix(LineRepository): remove a unnecessary function to build this project
    
    * feat(build.gradle, logback.xml, file-appender.xml, json-appender.xml): add Loggers
    
    * feat(AuthController, MapController, MemberController): log events
    
    * docs(README.md): add directories logs are saved
    
    * feat(build.gradle, applicatoin.properites): add properties for collecting Spring Actuator Metric
    
    * docs(README.md): add URL of my Cloudwatch dashboard
    
    * chore(file-appender.xml): change log format
    
    * chore(AuthController): log failed login
    
    * chore(file-appender.xml, json-appender.xml): add maxHistory and totalSizeCap

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -2,13 +2,19 @@ package nextstep.subway.map.ui;
 
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 @RestController
 public class MapController {
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
+
     private MapService mapService;
 
     public MapController(MapService mapService) {
@@ -17,6 +23,15 @@ public class MapController {
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
-        return ResponseEntity.ok(mapService.findPath(source, target));
+        jsonLogger.info("{}, {}",
+                kv("source", source),
+                kv("target", target)
+        );
+        PathResponse path = mapService.findPath(source, target);
+        jsonLogger.info("{}, {}",
+                kv("stations", path.getStations()),
+                kv("distance", path.getDistance())
+        );
+        return ResponseEntity.ok(path);
     }
 }
