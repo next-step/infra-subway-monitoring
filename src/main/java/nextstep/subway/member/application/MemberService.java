be7b6421560c3e0ae5e07d597df7f614bdcc1269
jiyeonghwang <gyp0288@naver.com>
2022-07-08 06:01:53 +0900

    feat: 회원가입, 로그인 등의 이벤트에 로깅을 설정

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,12 +4,16 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
+    private static final Logger log = LoggerFactory.getLogger(MemberService.class);
+
     private MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
@@ -17,6 +21,8 @@ public class MemberService {
     }
 
     public MemberResponse createMember(MemberRequest request) {
+        log.info("======= createMember =======");
+        log.info("email : {}", request.getEmail());
         Member member = memberRepository.save(request.toMember());
         return MemberResponse.of(member);
     }
@@ -27,6 +33,9 @@ public class MemberService {
     }
 
     public void updateMember(Long id, MemberRequest param) {
+        log.info("======= updateMember =======");
+        log.info("email : {}", param.getEmail());
+
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
         member.update(param.toMember());
     }
