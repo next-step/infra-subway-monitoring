5ce33462f6b9c5cc81bbfb1713eded00c76b42bc
kjkim <apio1130@naver.com>
2022-07-09 16:41:35 +0900

    test: smoke 테스트 실습

---
+++ k6/scripts/smoke.js
@@ -0,0 +1,58 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+// export let options = {
+//     vus: 1, // 1 user looping for 1 minute
+//     duration: '10s',
+//
+//     thresholds: {
+//         http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+//     },
+// };
+
+export let options = {
+    stages: [
+        { duration: '1m', target: 500 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '2m', target: 500 }, // stay at 100 users for 10 minutes
+        { duration: '10s', target: 0 }, // ramp-down to 0 users
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        // 'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    },
+};
+
+const BASE_URL = 'https://apio1130.r-e.kr/';
+const USERNAME = 'apio1130@naver.com';
+const PASSWORD = 'apio1130';
+
+export default function ()  {
+
+    var payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+
+    var params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${loginRes.json('accessToken')}`,
+        },
+    };
+    let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+    check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
+    sleep(1);
+};
