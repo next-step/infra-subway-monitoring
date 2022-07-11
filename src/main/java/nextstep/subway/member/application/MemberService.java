40ac34a14e4767cb4afc48f9003e37051c5224d4
chkim <cndghks0213@gmail.com>
2022-07-12 03:54:12 +0900

    feat: 회원 가입을 할 때 파일 로그를 남기는 기능 추가

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,13 +4,19 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @Service
 @Transactional
 public class MemberService {
-    private MemberRepository memberRepository;
+    private static final Logger log = LoggerFactory.getLogger("file");
+
+    private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -18,6 +24,7 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
+        log.info("{}", kv("신규 회원", member));
         return MemberResponse.of(member);
     }
 
