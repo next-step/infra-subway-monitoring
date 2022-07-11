71160604fe169fcba856e85070753deda4f879ec
chkim <cndghks0213@gmail.com>
2022-07-12 03:54:29 +0900

    feat: 로그인을 할 때 파일 로그를 남기는 기능 추가

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,14 +6,20 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @Service
 @Transactional
 public class AuthService {
-    private MemberRepository memberRepository;
-    private JwtTokenProvider jwtTokenProvider;
+    private static final Logger log = LoggerFactory.getLogger("file");
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public AuthService(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider) {
         this.memberRepository = memberRepository;
@@ -23,8 +29,8 @@ public class AuthService {
     public TokenResponse login(TokenRequest request) {
         Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(AuthorizationException::new);
         member.checkPassword(request.getPassword());
-
         String token = jwtTokenProvider.createToken(request.getEmail());
+        log.info("{}", kv("로그인 회원", member));
         return new TokenResponse(token);
     }
 
