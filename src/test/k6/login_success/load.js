df4da8627e2db2d5b9d59dded2c377c5a14a84ef
Dion <ksundong@gmail.com>
2022-08-27 22:51:05 +0900

    feat: 피드백 내용 반영(duration은 latency의 2배)

--- src/test/k6/login_success/load.js
+++ src/test/k6/login_success/load.js
@@ -9,7 +9,7 @@ export let options = {
   ],
 
   thresholds: {
-    http_req_duration: ['p(99)<100'], // 99% of requests must complete below 100ms
+    http_req_duration: ['p(99)<200'], // 99% of requests must complete below 200ms
   },
 };
 
