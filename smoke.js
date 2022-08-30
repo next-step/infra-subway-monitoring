0022f6e7f9c3e2bd9fee25bde14d23cc3cb94479
ingus26 <ingus26@gmail.com>
2022-08-31 00:55:39 +0900

    [2주차-2단계] 웹 부하 테스트

--- smoke.js
+++ smoke.js
@@ -6,7 +6,7 @@ export let options = {
   duration: '10s',
 
   thresholds: {
-    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    http_req_duration: ['p(99)<200'], // 99% of requests must complete below 1.5s
   },
 };
 
@@ -43,16 +43,4 @@ export default function ()  {
   let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
   check(myObjects, { 'retrieved member': (obj) => obj.id != 0 });
   sleep(1);
-};
-
-export let options = {
-  stages: [
-    { duration: '1m', target: 500 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: '2m', target: 500 }, // stay at 100 users for 10 minutes
-    { duration: '10s', target: 0 }, // ramp-down to 0 users
-  ],
-  thresholds: {
-    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
-    'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
-  },
 };
