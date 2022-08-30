897e053e8ac1b313bffb5979f3ff8cfe13dfbcb8
tyler.kim <writer0713@naver.com>
2022-08-30 12:21:59 +0900

    refactor: k6 stress test path.js stages vuser 변경
    - http_req_duration을 수정했기때문에 기존보다 vuser를 더 받을수 있다고 판단
    - vuser 최대 100까지로 수정

--- k6/scripts/stress/path.js
+++ k6/scripts/stress/path.js
@@ -13,9 +13,10 @@ export let options = {
   stages: [
     {duration: '5m', target: 9},
     {duration: '5m', target: 20},
-    {duration: '5m', target: 30},
     {duration: '5m', target: 40},
-    {duration: '5m', target: 50},
+    {duration: '5m', target: 60},
+    {duration: '5m', target: 80},
+    {duration: '5m', target: 100},
     {duration: '5m', target: 0},
   ],
   thresholds: {
