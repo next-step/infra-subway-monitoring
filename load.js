e21e26a6424ba6494cd0a7ab246de8a31f274e48
celinekim-nsuslab <celine.kim@nsuslab.com>
2022-08-31 12:10:25 +0900

    스크립트 수정

--- load.js
+++ load.js
@@ -2,10 +2,15 @@ import http from 'k6/http';
 import { check, group, sleep, fail } from 'k6';
 
 export let options = {
-  vus: 1, // 1 user looping for 1 minute
-  duration: '10s',
+  stages: [
+    { duration: '2m', target: 10 },
+    { duration: '2m', target: 100 },
+    { duration: '2m', target: 100 },
+    { duration: '2m', target: 100 },
+    { duration: '2m', target: 10 },
+  ],
   thresholds: {
-    http_req_duration: ['p(99)<200'], // 99% of requests must complete below 1.5s
+    http_req_duration: ['p(99)<200'],
   },
 };
 
--- stress.js
+++ stress.js
@@ -2,10 +2,16 @@ import http from 'k6/http';
 import { check, group, sleep, fail } from 'k6';
 
 export let options = {
-  vus: 1, // 1 user looping for 1 minute
-  duration: '10s',
+  stages: [
+    { duration: '1m', target: 50 },
+    { duration: '1m', target: 100 },
+    { duration: '1m', target: 150 },
+    { duration: '2m', target: 200 },
+    { duration: '2m', target: 150 },
+    { duration: '10s', target: 100 }
+  ],
   thresholds: {
-    http_req_duration: ['p(99)<200'], // 99% of requests must complete below 1.5s
+    http_req_duration: ['p(99)<200'],
   },
 };
 
