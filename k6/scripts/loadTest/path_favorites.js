7990773427699e49c5274b9d2b1a9ccb5b13ee96
JohnPrk <uo3641493@gmail.com>
2022-09-01 14:18:50 +0900

    update : load test -> path_favorites.js vuser 변경(vuser : 20)

--- k6/scripts/loadTest/path_favorites.js
+++ k6/scripts/loadTest/path_favorites.js
@@ -6,20 +6,25 @@ import { login, generateAuthorizationHeaderWith } from '../login.js';
 export const CounterErrors = new Counter('Errors');
 export let options = {
     stages: [
-        {duration: '15m', target: 31},
-        {duration: '15m', target: 31},
+        {duration: '15m', target: 20},
+        {duration: '15m', target: 20},
         {duration: '15m', target: 0},
     ],
 
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-        http_req_duration: ['p(99)<800'], // 99% of requests must complete below 0.8s
+        http_req_duration: ['p(99)<500'], // 99% of requests must complete below 0.5s
         'Errors': ['count<100'], // count of errors must be less than 100
     },
 };
 const BASE_URL = 'https://www.find-subway.p-e.kr';
 
 export default function ()  {
+
+    findTheBestRoute();
+    addToFavorites();
+}
+function findTheBestRoute() {
     var params = {
         headers: {
             'Content-Type': 'application/json',
@@ -34,6 +39,8 @@ export default function ()  {
         'find the best route in successfully': (resp) => resp.json().distance === 9,
     });
     sleep(1);
+}
+function addToFavorites(){
     const auth_header = generateAuthorizationHeaderWith(login());
     var payload = JSON.stringify({
         source: 6,
@@ -45,4 +52,4 @@ export default function ()  {
         'add To Favorites in successfully': (res) => res.status === 201,
     });
     sleep(1);
-};
+}
