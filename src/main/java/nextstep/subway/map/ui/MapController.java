8cb6ee4502cdec9148bff31d90a582deb5140e4b
orgojy <giveawesome@gmail.com>
2022-09-04 01:59:40 +0900

    Update MapController to log text into file, json

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -1,7 +1,13 @@
 package nextstep.subway.map.ui;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
+import java.util.List;
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+import nextstep.subway.station.dto.StationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
+
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
+
     private MapService mapService;
 
     public MapController(MapService mapService) {
@@ -17,6 +27,16 @@ public class MapController {
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
-        return ResponseEntity.ok(mapService.findPath(source, target));
+        fileLogger.info("source:{} -> target:{}", source, target);
+
+        PathResponse response = mapService.findPath(source, target);
+        List<StationResponse> stations = response.getStations();
+
+        jsonLogger.info("{}, {}",
+                kv("출발지", stations.get(0).getName()),
+                kv("도착지", stations.get(stations.size() - 1).getName())
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
