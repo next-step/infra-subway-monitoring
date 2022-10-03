c858465673f744df99985e458d4908d70136d260
LeeYongSeon <54692225+dldydtjs2965@users.noreply.github.com>
2022-10-03 23:08:04 +0900

    [1단계] 화면 응답 개선하기 (#508)

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require('path')
 
 const outputPath = path.resolve(__dirname, '../src/main/resources/static')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'production',
   output: {
     path: outputPath,
     filename: '[name].js'
-  }
+  },
+  plugins : [
+    new BundleAnalyzerPlugin()
+  ]
 }
