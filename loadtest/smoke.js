eccfbf0d9f89c5bb805c5b7d9eb89dc1ff4c6f34
kimseonghun <seonghun127@woowahan.com>
2022-10-11 22:15:34 +0900

    docs: smoke test 스크립트 변경

--- loadtest/smoke.js
+++ loadtest/smoke.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { check, group, sleep, fail } from 'k6';
+import {check, sleep} from 'k6';
 
 export let options = {
     vus: 1, // 1 user looping for 1 minute
@@ -12,28 +12,37 @@ export let options = {
 
 const BASE_URL = 'https://www.infra-subway-deploy.kro.kr';
 
-export default function ()  {
+// 역 범위 : 0~615
+function getRandomInt() {
+    return Math.floor(Math.random() * 616);
+}
 
-    var params = {
+export default function () {
+
+    let params = {
         headers: {
             'Content-Type': 'application/json',
         },
     };
 
     // 홈페이지 접근
-    let homepage = http.get(`${BASE_URL}`,  params);
+    let homepage = http.get(`${BASE_URL}`, params);
     check(homepage, {
         'homepage\'s status 200': (resp) => resp.status === 200,
     });
 
     // 경로 검색 탭 접근
-    let station = http.get(`${BASE_URL}/stations`,  params);
+    let station = http.get(`${BASE_URL}/stations`, params);
     check(station, {
         'station\'s status 200': (resp) => resp.status === 200,
     });
 
+    let stations = JSON.parse(station.body)
+    let source = stations[getRandomInt()].id
+    let target = stations[getRandomInt()].id
+
     // 출발역 & 도착역 선택 후 검색
-    let search = http.get(`${BASE_URL}/paths/?source=1&target=57`,  params);
+    let search = http.get(`${BASE_URL}/paths/?source=${source}&target=${target}`, params);
     check(search, {
         'search\'s status 200': (resp) => resp.status === 200,
     });
