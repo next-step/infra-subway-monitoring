40a8f0062e912e8486a865dd35937594fd6f0c74
TaeYang Lim <44702580+giraffelim@users.noreply.github.com>
2022-12-04 20:46:08 +0900

    Step2 부하 테스트 리뷰 요청 드립니다. (#532)
    * docs(README.md): 부하 테스트 전제조건 작성
    
    * docs(README.md): 부하 테스트 전제조건 중 대상 시스템 범위, 저장될 데이터 건수 및 크기 추가
    
    * feat(부하 테스트): smoke, load, stress 스크립트 및 결과 추가

---
+++ k6/stress/stress.js
@@ -0,0 +1,88 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '30s', target: 10 },
+        { duration: '1m', target: 30 },
+        { duration: '1m', target: 90 },
+        { duration: '1m', target: 180 },
+        { duration: '1m', target: 270 },
+        { duration: '1m', target: 180 },
+        { duration: '1m', target: 90 },
+        { duration: '1m', target: 30 },
+        { duration: '10s', target: 0 }
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<200'], // 99% of requests must complete below 200ms
+    },
+};
+
+const BASE_URL = 'https://web.giraffelim.kro.kr';
+const USERNAME = 'tester@tester.com';
+const PASSWORD = 'test123';
+
+export default function ()  {
+    mainPage();
+    const accessToken = login();
+    me(accessToken);
+    pathSearchPage();
+    pathSearch(accessToken);
+};
+
+function mainPage() {
+    let mainPageResponse = http.get(`${BASE_URL}`);
+
+    check(mainPageResponse, {
+        'get main page successfully': (response) => response.status === 200,
+    });
+}
+
+function login() {
+    const payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+
+    return loginRes.json('accessToken');
+}
+
+function me(accessToken) {
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    };
+    let myObjects = http.get(`${BASE_URL}/members/me`, authHeaders).json();
+
+    check(myObjects, { 'retrieved member successfully': (obj) => obj.id != 0 });
+}
+
+function pathSearchPage() {
+    let findPathPageResponse = http.get(`${BASE_URL}/path`);
+
+    check(findPathPageResponse, {
+        'get search path page successfully': (response) => response.status === 200,
+    });
+}
+
+function pathSearch(accessToken) {
+    let authHeaders = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    };
+    let findPathResponse = http.get(`${BASE_URL}/paths?source=50&target=106`, authHeaders);
+
+    check(findPathResponse, {'search shortest path successfully': (response) => response.status === 200})
+}
