4b5b876d0ffb363e579ec43bbea889ed2a708894
DoGyeong Seo <dgseo8981@gmail.com>
2022-12-05 00:06:17 +0900

    🚀 2단계 - 부하테스트 PR (#536)
    * docs: 경쟁사 비교 분석 자료 추가
    
    * fix: 실습용 코드 제거
    
    * test: AWS에서 fail 나와서 Disabled 처리
    
    * test: 원복
    
    * feat: 운영 서버에서 부하 테스트에 필요한 디비 연결 정보 추가
    
    * fix: profile prod 추가
    
    * docs: smoke.js, load.js, stress.js 스크립트 작성

--- src/main/java/nextstep/subway/line/domain/LineRepository.java
+++ src/main/java/nextstep/subway/line/domain/LineRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
-
-    @Query(value = "SELECT * FROM line WHERE SLEEP(3)", nativeQuery = true)
-    List<Line> findAll();
 }
