7a54443c4176fa5ad4677d9eca965626253904be
giraffelim <taeyang7704@naver.com>
2022-12-06 00:54:51 +0900

    feat(MemberService): 회원가입 시 로깅 추가

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,13 +4,17 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
-    private MemberRepository memberRepository;
+    private static final Logger logger = LoggerFactory.getLogger("file");
+
+    private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -18,6 +22,7 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
+        logger.info("[유저 생성] memberId={}, email={}", member.getId(), member.getEmail());
         return MemberResponse.of(member);
     }
 
