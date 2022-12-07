baef6d6710e4892704389e022333a7d70ad301a4
yoonmin-kim <rladbsals23@gmail.com>
2022-12-08 01:11:54 +0900

    feat(LOG): 로그 설정하기(이벤트/json 로깅, nginx Access Log)

--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 ### 로깅, 모니터링
 * 로그 설정하기
-  * [ ] Application Log 파일로 저장하기
-    * [ ] 회원가입, 로그인 등의 이벤트에 로깅을 설정
+  * [x] Application Log 파일로 저장하기
+    * [x] 회원가입, 로그인 등의 이벤트에 로깅을 설정
       * 로그에 사용자의 전화번호, 계좌번호, 패스워드, 주소 같은 개인정보 제외
-    * [ ] 경로찾기 등의 이벤트 로그를 JSON으로 수집
-  * [ ] Nginx Access Log 설정하기
+    * [x] 경로찾기 등의 이벤트 로그를 JSON으로 수집
+  * [x] Nginx Access Log 설정하기
 * Cloudwatch 로 모니터링
   * [ ] Cloudwatch 로 로그 수집하기
   * [ ] Cloudwatch 로 메트릭 수집하기
--- src/main/java/nextstep/subway/common/LogAspect.java
+++ src/main/java/nextstep/subway/common/LogAspect.java
@@ -5,37 +5,49 @@ import java.util.UUID;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogAspect {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger("subway");
     ThreadLocal<String> threadUUID = new ThreadLocal<>();
 
     @Around("execution(* nextstep.subway..*.*(..)) && !@target(nextstep.subway.common.NoLogging)")
     public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
-
-        String uuid = makeUUID();
-
-        long startTime = System.currentTimeMillis();
-        System.out.println("[" + uuid + "][" + joinPoint.getSignature() + "] 시작");
-        Arrays.stream(joinPoint.getArgs()).forEach(param -> System.out.println("[" + uuid + "] param = " + param));
+        long startTime = startLogging(joinPoint);
         Object result = joinPoint.proceed();
-        if (result != null) {
-            System.out.println("[" + uuid + "] result = " + result);
-        }
-        System.out.println("[" + uuid + "][" + joinPoint.getSignature() + "] 끝 ["
-                + (System.currentTimeMillis() - startTime) + "(ms)]");
+        endLogging(joinPoint, startTime, result);
+
         return result;
     }
 
-    private String makeUUID() {
-        String[] uuid = UUID.randomUUID().toString().split("-");
-        if (threadUUID.get() != null) {
-            return threadUUID.get();
+    private void endLogging(ProceedingJoinPoint joinPoint, long startTime, Object result) {
+        if (result != null) {
+            LOGGER.info("[{}] result = {}", threadUUID.get(), result);
         }
-        threadUUID.set(uuid[0]);
-        return uuid[0];
+        LOGGER.info("[{}][{}] 끝 [{}(ms)]", threadUUID.get(), joinPoint.getSignature(),
+                (System.currentTimeMillis() - startTime));
+    }
+
+    private long startLogging(ProceedingJoinPoint joinPoint) {
+        makeUUID();
+        long startTime = System.currentTimeMillis();
+        LOGGER.info("[{}][{}] 시작", threadUUID.get(), joinPoint.getSignature());
+        if (joinPoint.getArgs() != null && joinPoint.getArgs().length != 0) {
+            Arrays.stream(joinPoint.getArgs())
+                    .forEach(param -> LOGGER.info("[{}] param = {}", threadUUID.get(), param));
+        }
+        return startTime;
+    }
+
+    private void makeUUID() {
+        if (threadUUID.get() == null) {
+            String[] uuid = UUID.randomUUID().toString().split("-");
+            threadUUID.set(uuid[0]);
+        }
     }
 }
