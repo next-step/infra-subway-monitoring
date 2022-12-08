d874b41738a4756957dae87cb4732b5c77a49362
Haseop Lee <haservi@naver.com>
2022-12-09 00:20:42 +0900

    feat(Logger): 로그 설정 파일 분리 및 회원가입, 로그인, 경로 조회 로그 출력 추가

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,10 +3,7 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
-    private static final Logger log = LoggerFactory.getLogger(Controller.class);
-    private static final Logger fileLogger = LoggerFactory.getLogger("file");
-
-
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
-        fileLogger.info("파일 로깅 입니다.");
-        log.error("An ERROR Message");
         TokenResponse token = authService.login(request);
         return ResponseEntity.ok().body(token);
     }
