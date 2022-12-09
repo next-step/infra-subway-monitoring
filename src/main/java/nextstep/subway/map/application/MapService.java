f0cbaeb59c6091225072e94526eee7454c7e5d75
ilmare-cbk <cbk104325@gmail.com>
2022-12-09 22:39:36 +0900

    feat(LogAop.java): Aop를 통한 로깅에 대한 관심사 분리

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -7,19 +7,14 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static net.logstash.logback.argument.StructuredArguments.kv;
-
 @Service
 @Transactional
 public class MapService {
-    private final static Logger LOGGER = LoggerFactory.getLogger("json");
     private LineService lineService;
     private StationService stationService;
     private PathService pathService;
@@ -35,12 +30,6 @@ public class MapService {
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
-
-        LOGGER.info("Found path results : {}, {}, {}",
-                kv("sourceStation", source),
-                kv("targetStation", target),
-                kv("path", subwayPath.getStations().toString()));
-
         return PathResponseAssembler.assemble(subwayPath);
     }
 }
--- src/main/java/nextstep/subway/member/dto/MemberResponse.java
+++ src/main/java/nextstep/subway/member/dto/MemberResponse.java
@@ -31,4 +31,12 @@ public class MemberResponse {
     public Integer getAge() {
         return age;
     }
+
+    @Override
+    public String toString() {
+        return "MemberResponse{" +
+                "id=" + id +
+                ", age=" + age +
+                '}';
+    }
 }
