044703526ced1c7b16db5c1b682546fb4d2db9ff
sawhong <sawhong@gmarket.com>
2022-12-09 20:20:30 +0900

    fix: 테스트 재진행

--- k6/load/Load.js
+++ k6/load/Load.js
@@ -1,26 +1,65 @@
 // # Stress.js
 import http from 'k6/http';
-import { check, group, sleep, fail } from 'k6';
+import {check} from 'k6';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export let options = {
-  stages: [
-    { duration: '30s', target: 6 },
-    { duration: '1m', target: 13 },
-    { duration: '3m', target: 19 },
-    { duration: '1m', target: 13 },
-    { duration: '10s', target: 0 },
-  ],
   thresholds: {
-    http_req_duration: ['p(99)<300'],
+    http_req_duration: ['p(95)<300'],
   },
+  ext: {
+    loadimpact: {
+      projectID: "******",
+      name: "LOAD TEST"
+    }
+  },
+  scenarios: {
+    loginTest: {
+      executor: 'ramping-arrival-rate',
+      exec: 'loginTest',
+      preAllocatedVUs: 19,
+      stages: [
+        { duration: '30s', target: 6 },
+        { duration: '1m', target: 13 },
+        { duration: '1m', target: 17 },
+        { duration: '3m', target: 19 },
+        { duration: '1m', target: 13 },
+        { duration: '1m', target: 13 },
+        { duration: '10s', target: 0 },
+      ],
+    },
+
+    noLoginTest: {
+      executor: 'ramping-arrival-rate',
+      exec: 'noLoginTest',
+      preAllocatedVUs: 19,
+      stages: [
+        { duration: '30s', target: 6 },
+        { duration: '1m', target: 13 },
+        { duration: '1m', target: 17 },
+        { duration: '3m', target: 19 },
+        { duration: '1m', target: 13 },
+        { duration: '1m', target: 13 },
+        { duration: '10s', target: 0 },
+      ]
+    },
+  }
 };
+
 
 const BASE_URL = 'https://sawooook-webservice.p-e.kr';
 const USERNAME = 'test@naver.com';
 const PASSWORD = 'test1234';
 
-export default function ()  {
+function sourceRandomNumber() {
+  return randomIntBetween(4, 5)
+}
 
+function targetRandomNumber() {
+  return randomIntBetween(6, 8)
+}
+
+function loginPage() {
   const payload = JSON.stringify({
     email: USERNAME,
     password: PASSWORD,
@@ -32,34 +71,49 @@ export default function ()  {
     },
   };
 
-
   // 로그인
-  const loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+  const response = http.post(`${BASE_URL}/login/token`, payload, params);
 
-  check(loginRes, {
+  check(response, {
     '로그인 페이지': (resp) => resp.json('accessToken') !== '',
-  });
+  }, {tags: '로그인 페이지'});
 
 
+  return {
+    headers: {
+      Authorization: `Bearer ${response.json('accessToken')}`,
+    },
+  };
+}
+
+function searchPath() {
+  let source = sourceRandomNumber();
+  let target = targetRandomNumber();
+
+  let findPathResponse = http.get(`${BASE_URL}/paths?source=${source}&target=${target}`);
+
+  check(findPathResponse, {
+    '경로 검색하기': (response) => response.status === 200,
+  }, {tags: '경로 검색하기'});
+}
+
+function searchPathPage() {
   // 경로 페이지 검색
   const getPath = http.get(`${BASE_URL}/path`);
 
   check(getPath, {
     '경로 검색 페이지': (response) => response.status === 200,
-  });
+  }, {tags: '경로 검색 페이지'});
+}
 
 
-  // 인증헤더
-  let authHeaders = {
-    headers: {
-      Authorization: `Bearer ${loginRes.json('accessToken')}`,
-    },
-  };
+export function loginTest() {
+  loginPage();
+  searchPath();
+  searchPathPage();
+}
 
-  let findPathResponse = http.get(`${BASE_URL}/paths?source=5&target=8`, authHeaders);
-
-  check(findPathResponse, {
-    '경로 검색하기': (response) => response.status === 200,
-  });
-};
-
+export function noLoginTest() {
+  searchPath();
+  searchPathPage();
+}
--- k6/smoke/Smoke.js
+++ k6/smoke/Smoke.js
@@ -1,14 +1,34 @@
 // # smoke.js
 import http from 'k6/http';
 import {check} from 'k6';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
 
 export let options = {
-  vus: 19,
-  duration: '60s',
-
   thresholds: {
-    http_req_duration: ['p(99)<300'],
+    http_req_duration: ['p(95)<300'],
   },
+  ext: {
+    loadimpact: {
+      projectID: "******",
+      name: "SMOKE TEST"
+    }
+  },
+  scenarios: {
+    loginTest: {
+      executor: 'constant-vus',
+      exec: 'loginTest',
+      vus: 5,
+      duration: '60s',
+    },
+
+    noLoginTest: {
+      executor: 'constant-vus',
+      exec: 'noLoginTest',
+      vus: 12,
+      duration: '60s',
+    },
+  }
 };
 
 
@@ -16,8 +36,15 @@ const BASE_URL = 'https://sawooook-webservice.p-e.kr';
 const USERNAME = 'test@naver.com';
 const PASSWORD = 'test1234';
 
-export default function () {
+function sourceRandomNumber() {
+  return randomIntBetween(4, 5)
+}
 
+function targetRandomNumber() {
+  return randomIntBetween(6, 8)
+}
+
+function loginPage() {
   const payload = JSON.stringify({
     email: USERNAME,
     password: PASSWORD,
@@ -30,33 +57,48 @@ export default function () {
   };
 
   // 로그인
-  const loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+  const response = http.post(`${BASE_URL}/login/token`, payload, params);
 
-  check(loginRes, {
+  check(response, {
     '로그인 페이지': (resp) => resp.json('accessToken') !== '',
-  });
+  }, {tags: '로그인 페이지'});
 
 
+  return {
+    headers: {
+      Authorization: `Bearer ${response.json('accessToken')}`,
+    },
+  };
+}
+
+function searchPath() {
+  let source = sourceRandomNumber();
+  let target = targetRandomNumber();
+
+  let findPathResponse = http.get(`${BASE_URL}/paths?source=${source}&target=${target}`);
+
+  check(findPathResponse, {
+    '경로 검색하기': (response) => response.status === 200,
+  }, {tags: '경로 검색하기'});
+}
+
+function searchPathPage() {
   // 경로 페이지 검색
   const getPath = http.get(`${BASE_URL}/path`);
 
   check(getPath, {
     '경로 검색 페이지': (response) => response.status === 200,
-  });
+  }, {tags: '경로 검색 페이지'});
+}
 
 
-  // 인증헤더
-  let authHeaders = {
-    headers: {
-      Authorization: `Bearer ${loginRes.json('accessToken')}`,
-    },
-  };
+export function loginTest() {
+  loginPage();
+  searchPath();
+  searchPathPage();
+}
 
-
-  let findPathResponse = http.get(`${BASE_URL}/paths?source=5&target=8`, authHeaders);
-
-  check(findPathResponse, {
-    '경로 검색하기': (response) => response.status === 200,
-  });
-};
-
+export function noLoginTest() {
+  searchPath();
+  searchPathPage();
+}
