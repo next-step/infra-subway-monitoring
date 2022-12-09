99a12ac4a08b6ca4a1d43451b80d4d9c4f0b4e49
jeongmin <jeongmin@nhndooray.com>
2022-12-09 09:25:03 +0900

    #w2step2 - MvcConfig 추가
    - HTTP 캐시 정책 사용

---
+++ src/main/java/nextstep/subway/web/MvcConfig.java
@@ -0,0 +1,22 @@
+package nextstep.subway.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        CacheControl cacheControl = CacheControl.maxAge(7, TimeUnit.DAYS)
+                .mustRevalidate();
+
+        registry.addResourceHandler("**/.js", "**/.png")
+                .addResourceLocations("classpath:/static")
+                .setCacheControl(cacheControl);
+    }
+}
