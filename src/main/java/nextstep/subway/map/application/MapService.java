b6e31be67b4b25b2c96c55b20c30b8bea5e66448
CHEESE <81912261+ychxexn@users.noreply.github.com>
2022-12-10 00:38:01 +0900

    refactor: 로깅 추가

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -7,17 +7,24 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 @Service
 @Transactional
 public class MapService {
-    private LineService lineService;
-    private StationService stationService;
-    private PathService pathService;
+    private static final Logger log = LoggerFactory.getLogger(MapService.class);
+    private static final Logger jsonLogger = LoggerFactory.getLogger("json");
+
+    private final LineService lineService;
+    private final StationService stationService;
+    private final PathService pathService;
 
     public MapService(LineService lineService, StationService stationService, PathService pathService) {
         this.lineService = lineService;
@@ -30,6 +37,14 @@ public class MapService {
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
+
+        log.info("경로 조회 :: {} -> {}", sourceStation.getName(), targetStation.getName());
+        jsonLogger.info("{}, {}, {}, {}",
+                kv("출발지", sourceStation.getName()),
+                kv("도착지", targetStation.getName()),
+                kv("경유역", subwayPath.getStations()),
+                kv("거리", subwayPath.calculateDistance())
+        );
 
         return PathResponseAssembler.assemble(subwayPath);
     }
--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,13 +4,17 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
-    private MemberRepository memberRepository;
+    private static final Logger log = LoggerFactory.getLogger(MemberService.class);
+
+    private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -18,6 +22,7 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
+        log.info("멤버 생성 성공 :: member id={}", member.getId());
         return MemberResponse.of(member);
     }
 
@@ -29,6 +34,7 @@ public class MemberService {
     public void updateMember(Long id, MemberRequest param) {
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
         member.update(param.toMember());
+        log.info("멤버 수정 성공 :: member id={}", member.getId());
     }
 
     public void deleteMember(Long id) {
