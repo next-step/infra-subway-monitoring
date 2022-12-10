a21a787295c18f0dc6c5be0122a9859ad49da61b
CHEESE <ychxexn@gmail.com>
2022-12-10 18:54:38 +0900

    [Step3] 로깅, 모니터링 (#592)
    * feat: 로깅을 위한 설정 파일 추가
    
    * fix: 문제 코드 수정
    
    * refactor: 로깅 추가
    
    * feat: 설정 파일 추가
    
    * feat: Spring Actuator Metric 수집
    
    * refactor: 파일 로깅으로 수정
    
    * docs(README.md): Step3 답변 작성

--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,14 +6,18 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
-    private MemberRepository memberRepository;
-    private JwtTokenProvider jwtTokenProvider;
+    private static final Logger log = LoggerFactory.getLogger("file");
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public AuthService(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider) {
         this.memberRepository = memberRepository;
@@ -25,11 +29,13 @@ public class AuthService {
         member.checkPassword(request.getPassword());
 
         String token = jwtTokenProvider.createToken(request.getEmail());
+        log.info("로그인 성공 :: member id={}", member.getId());
         return new TokenResponse(token);
     }
 
     public LoginMember findMemberByToken(String credentials) {
         if (!jwtTokenProvider.validateToken(credentials)) {
+            log.info("토큰 유효성 체크 실패 :: credentials={}", credentials);
             throw new AuthorizationException();
         }
 
