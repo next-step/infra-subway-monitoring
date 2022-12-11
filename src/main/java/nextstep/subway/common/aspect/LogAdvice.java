f1b928195bc06db837fe69f1ef2ed6da0e5091be
aksgudwns <114580557+aksgudwns@users.noreply.github.com>
2022-12-11 10:16:34 +0900

    Step3 - 로깅, 모니터링 리뷰요청 드립니다. (#596)
    * feat - log 설정 추가
    
    - 회원가입 이벤트 로그 구현
    - 로그인 이벤트 로그 구현
    - 길찾기 이벤트 json 로그 구현
    - aop를 사용한 controller in/out 로그 구현
    
    * feat - Spring Actuator Metric 수집 환경설정 추가
    
    * fix - Responsebody가 없는 경우 오류수정
    
    * doc - 서버내 로깅경로, 대시보드 url 추가

---
+++ src/main/java/nextstep/subway/common/aspect/LogAdvice.java
@@ -0,0 +1,53 @@
+package nextstep.subway.common.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAdvice {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("file");
+
+    @Pointcut("execution(* nextstep.subway..ui.*.*(..))")
+    private void advicePoint() {}
+
+    @Before("advicePoint()")
+    public void logBefore(JoinPoint joinPoint) {
+        String controller =  joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String method = ((MethodSignature) joinPoint.getSignature()).toString();
+        String parameters = "";
+        for(Object arg : joinPoint.getArgs()) {
+            parameters += (arg.getClass().cast(arg).toString() + ",");
+        }
+        if(parameters.length() > 1) {
+            parameters = parameters.substring(0, parameters.length() - 1);
+        }
+
+        LOGGER.info("Controller in -> Controller name : {}, Method name : {}, parameters : {}",
+                controller, method, parameters);
+    }
+
+    @AfterReturning(value = "advicePoint()", returning = "obj")
+    public void afterLogging(JoinPoint joinPoint, Object obj) {
+        if (obj instanceof ResponseEntity) {
+            ResponseEntity response = (ResponseEntity) obj;
+            Object responseBody = response.getBody();
+            if(responseBody != null) {
+                LOGGER.info("Controller out -> status={}, res={}", response.getStatusCodeValue(),
+                        responseBody.getClass().cast(responseBody).toString());
+            }
+            LOGGER.info("Controller out -> status={}", response.getStatusCodeValue());
+
+        }
+    }
+
+}
