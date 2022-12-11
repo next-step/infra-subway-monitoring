a4a5c37c7fb7fe52414437e218c308ba5127de5c
하수종 <hsz3855@gmail.com>
2022-12-11 16:26:32 +0900

    feat : 회원 관련 메소드에 file logger 적용

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -4,12 +4,16 @@ import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
+
+    private static final Logger fileLogger = LoggerFactory.getLogger("file");
     private MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
@@ -18,6 +22,7 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
+        fileLogger.info("{} 사용자 등록 성공" , member.getId());
         return MemberResponse.of(member);
     }
 
@@ -29,9 +34,11 @@ public class MemberService {
     public void updateMember(Long id, MemberRequest param) {
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
         member.update(param.toMember());
+        fileLogger.info("{} 사용자 수정 성공" , member.getId());
     }
 
     public void deleteMember(Long id) {
         memberRepository.deleteById(id);
+        fileLogger.info("{} 사용자 탈퇴 성공" , id);
     }
 }
