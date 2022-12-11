468b7dd04338a0edaf4bb308fad11144d5bf16f1
choiyongseok90 <badaelephant>
2022-12-12 02:28:31 +0900

    refactor: 요청, 응답객체 AOP에 추가

--- src/main/java/nextstep/subway/aop/LogAop.java
+++ src/main/java/nextstep/subway/aop/LogAop.java
@@ -1,11 +1,13 @@
 package nextstep.subway.aop;
 
+import java.util.Arrays;
 import javax.servlet.http.HttpServletRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -27,17 +29,18 @@ public class LogAop {
         String requestURI = request.getRequestURI();
         String controllerName = pjp.getSignature().getDeclaringType().getSimpleName();
         String methodName = pjp.getSignature().getName();
-
+        String arg = Arrays.toString(pjp.getArgs());
         long startAt = System.currentTimeMillis();
 
-        logger.info("----------> [REQUEST] : [{}] {} = {} - {}",
-                httpMethod, requestURI, controllerName, methodName);
-
+        logger.info(">> [요청] : [{}] {} = {} - {} >> 요청 객체 : {}",
+                httpMethod, requestURI, controllerName, methodName, arg);
         Object result = pjp.proceed();
-
         long endAt = System.currentTimeMillis();
-
-        logger.info("----------> [RESPONSE] : [{}] {} = {} - {} ({}ms)",
+        if(result instanceof ResponseEntity){
+            logger.info(">> [응답] : [{}] {} = {} - {} ({}ms) >> 응답 객체 : {}",
+                    httpMethod, requestURI, controllerName, methodName, endAt - startAt, (ResponseEntity)result);
+        }
+        logger.info(">> [응답] : [{}] {} = {} - {} ({}ms) >> 응답 객체 : 없음",
                 httpMethod, requestURI, controllerName, methodName, endAt - startAt);
 
         return result;
--- src/main/java/nextstep/subway/map/dto/PathResponseAssembler.java
+++ src/main/java/nextstep/subway/map/dto/PathResponseAssembler.java
@@ -19,4 +19,6 @@ public class PathResponseAssembler {
 
     private PathResponseAssembler() {
     }
+
+
 }
