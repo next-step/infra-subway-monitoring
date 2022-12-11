a44cdcb36b17a1bf54548c76c9f908bd94dcf3d0
velcronicity <velcronicity@gmail.com>
2022-12-11 20:04:46 +0900

    feat: 로그 설정

--- src/main/java/nextstep/subway/member/application/MemberService.java
+++ src/main/java/nextstep/subway/member/application/MemberService.java
@@ -1,15 +1,20 @@
 package nextstep.subway.member.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
 import nextstep.subway.member.dto.MemberRequest;
 import nextstep.subway.member.dto.MemberResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class MemberService {
+    private static final Logger log = LoggerFactory.getLogger("file");
+
     private MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
@@ -18,6 +23,7 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest request) {
         Member member = memberRepository.save(request.toMember());
+        log.info("[회원 생성] 아이디:[{}], 이메일:[{}]", member.getId(), member.getEmail());
         return MemberResponse.of(member);
     }
 
@@ -28,10 +34,12 @@ public class MemberService {
 
     public void updateMember(Long id, MemberRequest param) {
         Member member = memberRepository.findById(id).orElseThrow(RuntimeException::new);
+        log.info("[회원 수정] 아이디:[{}], 이메일:[{}]", member.getId(), member.getEmail());
         member.update(param.toMember());
     }
 
     public void deleteMember(Long id) {
         memberRepository.deleteById(id);
+        log.info("[회원 삭제] 아이디:[{}]", id);
     }
 }
