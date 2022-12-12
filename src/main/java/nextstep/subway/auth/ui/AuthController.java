5dd4f7d552e9c8463683fba5f4dd432bdb6ef592
hahoho87 <bbaing4564@gmail.com>
2022-12-13 01:47:51 +0900

    feat: aop 활용한 로깅 적용
    - 회원 관련 기능: file
    - 기타 기능: json

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -3,6 +3,8 @@ package nextstep.subway.auth.ui;
 import nextstep.subway.auth.application.AuthService;
 import nextstep.subway.auth.dto.TokenRequest;
 import nextstep.subway.auth.dto.TokenResponse;
+import nextstep.subway.common.logging.Logging;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +19,7 @@ public class AuthController {
     }
 
     @PostMapping("/login/token")
+    @Logging(type = Logging.LoggingType.FILE)
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
         TokenResponse token = authService.login(request);
         return ResponseEntity.ok().body(token);
