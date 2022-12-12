6f6add1e3b085948b4af6099f7747b518c2b6d8a
paki1019 <paki1019@gmail.com>
2022-12-12 17:30:11 +0900

    Step3 - 로깅, 모니터링 리뷰 요청드립니다. (#606)
    * feat: 테스트 실패 쿼리 제거
    
    * docs: step3 내용 추가
    
    * feat: 로그 설정하기 - Application Log 파일로 저장하기
    
    * feat: 로그 설정하기 - Nginx Access Log 설정하기
    
    * feat: 로그 설정하기 - CloudWatch로 모니터링
    
    * feat: 로그 설정하기 - CloudWatch로 모니터링 - docs에 url 추가

--- src/main/java/nextstep/subway/auth/ui/AuthController.java
+++ src/main/java/nextstep/subway/auth/ui/AuthController.java
@@ -1,16 +1,21 @@
 package nextstep.subway.auth.ui;
 
-import nextstep.subway.auth.application.AuthService;
-import nextstep.subway.auth.dto.TokenRequest;
-import nextstep.subway.auth.dto.TokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import nextstep.subway.auth.application.AuthService;
+import nextstep.subway.auth.dto.TokenRequest;
+import nextstep.subway.auth.dto.TokenResponse;
+
 @RestController
 public class AuthController {
-    private AuthService authService;
+    private final static Logger logger = LoggerFactory.getLogger("file");
+
+    private final AuthService authService;
 
     public AuthController(AuthService authService) {
         this.authService = authService;
@@ -19,6 +24,7 @@ public class AuthController {
     @PostMapping("/login/token")
     public ResponseEntity<TokenResponse> login(@RequestBody TokenRequest request) {
         TokenResponse token = authService.login(request);
+        logger.info("로그인 성공 - token: {}", token.getAccessToken());
         return ResponseEntity.ok().body(token);
     }
 }
