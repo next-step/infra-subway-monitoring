7d2fc9c82cb9ebd7ee3afebb2662f0126ed7c57a
jisu1211 <dbdnzl1211@gmail.com>
2022-12-12 10:57:43 +0900

    feat: 회원가입, 로그인, 경로 검색에 대한 로깅 추가 feat: Spring Actuator Metric 수집 추가

--- src/main/java/nextstep/subway/map/application/PathService.java
+++ src/main/java/nextstep/subway/map/application/PathService.java
@@ -7,13 +7,20 @@ import nextstep.subway.map.domain.SubwayPath;
 import nextstep.subway.station.domain.Station;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 @Service
 public class PathService {
+    private static final Logger FILE_LOGGER = LoggerFactory.getLogger("file");
+    private static final Logger JSON_LOGGER = LoggerFactory.getLogger("json");
+
     public SubwayPath findPath(List<Line> lines, Station source, Station target) {
         SubwayGraph graph = new SubwayGraph(SectionEdge.class);
         graph.addVertexWith(lines);
@@ -22,8 +29,16 @@ public class PathService {
         // 다익스트라 최단 경로 찾기
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         GraphPath<Station, SectionEdge> path = dijkstraShortestPath.getPath(source, target);
+        SubwayPath subwayPath = convertSubwayPath(path);
 
-        return convertSubwayPath(path);
+        JSON_LOGGER.info("{}, {}, {}, {}",
+                kv("출발지", source.getName()),
+                kv("도착지", target.getName()),
+                kv("경로", subwayPath.getStations().toString()),
+                kv("거리", subwayPath.calculateDistance())
+        );
+
+        return subwayPath;
     }
 
     private SubwayPath convertSubwayPath(GraphPath graphPath) {
