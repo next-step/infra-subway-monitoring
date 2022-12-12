55421be261f5642bc0e39c09299430e77460f329
velcronicity <114590853+velcronicity@users.noreply.github.com>
2022-12-12 11:49:50 +0900

    Step3 pr 요청드립니다. (#611)
    * docs: 요구사항 정리
    
    * fix: sleep 쿼리
    
    * feat: 로그 설정
    
    * feat: metric
    
    * docs: step3

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -1,5 +1,15 @@
 package nextstep.subway.map.application;
 
+import static java.util.stream.Collectors.*;
+import static net.logstash.logback.argument.StructuredArguments.*;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.map.domain.SubwayPath;
@@ -7,14 +17,12 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
 public class MapService {
+    private static final Logger log = LoggerFactory.getLogger("json");
+
     private LineService lineService;
     private StationService stationService;
     private PathService pathService;
@@ -31,6 +39,12 @@ public class MapService {
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
 
+        log.info("{}, {}, {}, {}",
+            kv("출발지", sourceStation.getName()),
+            kv("도착지", targetStation.getName()),
+            kv("경로", subwayPath.getStations().stream().map(Station::getName).collect(toList())),
+            kv("거리", subwayPath.calculateDistance())
+        );
         return PathResponseAssembler.assemble(subwayPath);
     }
 }
