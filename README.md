97a4370b92f76f5fc455fe56928726f387fdaa0c
hyeongju kim <hjkim880527@gmail.com>
2022-12-13 19:31:03 +0900

    3단계 - 로깅, 모니터링 PR 요청합니다 (#628)
    * refactor: 출퇴근 피크시간대의 target을 현실성있게 늘림
    
    * docs: 3단계 요구사항 정의
    
    * fix: sleep 쿼리 제거
    
    * chore: application proterties 운영 파일 추가
    
    * chore: logback 라이브러리 추가
    
    * chore: Apple apple silicon Mac에서 이미지 설치 할 수 있도록 설정 추가
    
    * feat: logback 설정 추가
    
    * feat: 중요 이벤트 로깅 추가
    
    * docs: logback 설정 진행 상황 체크
    
    * docs: Cloudwatch로 로그 수집하기 완료
    
    * chore: cloudwatch 로그 세팅
    
    * docs: 대시보드 구성 완료
    
    * docs: cloudwatch namespace 추가
    
    * chore: spring Cloudwatch로 로그 수집하기
    
    * chore: Spring Actuator Metric 라이브러리 추가
    
    * docs: applicatoin 경로 추가

--- README.md
+++ README.md
@@ -121,6 +121,21 @@ npm run dev
 ---
 
 ### 3단계 - 로깅, 모니터링
+- [x] 애플리케이션 진단하기 실습을 진행해보고 문제가 되는 코드 수정
+- [x] 로그 설정
+  - [x] Application Log 파일로 저장하기
+    - [x] 회원가입, 로그인 등의 이벤트에 로깅을 설정
+    - [x] 경로찾기 등의 이벤트 로그를 JSON으로 수집
+  - [x] Nginx Access Log 설정하기
+- [ ] Cloudwatch로 모니터링
+  - [x] Cloudwatch로 로그 수집하기
+  - [x] Cloudwatch로 메트릭 수집하기
+  - [x] USE 방법론을 활용하기 용이하도록 대시보드 구성
 1. 각 서버내 로깅 경로를 알려주세요
+- syslog : /var/log/syslog
+- nginx access log : /var/log/nginx/access.log
+- nginx error log : /var/log/nginx/error.log
+- applicatoin log : /home/ubuntu/nextstep/infra-subway-monitoring/log/*.log 
 
 2. Cloudwatch 대시보드 URL을 알려주세요
+- https://ap-northeast-2.console.aws.amazon.com/cloudwatch/home?region=ap-northeast-2#dashboards:name=hyeongjukim-dashboard
--- src/main/java/nextstep/subway/auth/application/AuthService.java
+++ src/main/java/nextstep/subway/auth/application/AuthService.java
@@ -6,12 +6,15 @@ import nextstep.subway.auth.dto.TokenResponse;
 import nextstep.subway.auth.infrastructure.JwtTokenProvider;
 import nextstep.subway.member.domain.Member;
 import nextstep.subway.member.domain.MemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AuthService {
+    private final static Logger logger = LoggerFactory.getLogger("file");
     private MemberRepository memberRepository;
     private JwtTokenProvider jwtTokenProvider;
 
@@ -21,6 +24,7 @@ public class AuthService {
     }
 
     public TokenResponse login(TokenRequest request) {
+        logger.info("[login] email : {}", request.getEmail());
         Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(AuthorizationException::new);
         member.checkPassword(request.getPassword());
 
