29f8e4a651eeca353343ff21595b338f99a8f834
지영진 <youngjin.jee@nhn.com>
2022-12-16 23:01:05 +0900

    docs: 부하테스트 전제조건 및 Smoke,Load,Stress 테스트 스크립트, 결과 공유

---
+++ k6/stress.js
@@ -0,0 +1,65 @@
+import http from 'k6/http';
+import {check, group, sleep, fail} from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '10s', target: 0 },
+        { duration: '10s', target: 77 },
+        { duration: '10s', target: 144 },
+        { duration: '10s', target: 200 },
+        { duration: '10s', target: 300 },
+        { duration: '10s', target: 0 },
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<500'], // 99% of requests must complete below 0.5s
+    },
+};
+
+const BASE_URL = 'https://ganjinajae.n-e.kr';
+const USERNAME = 'email@email.com';
+const PASSWORD = 'password';
+
+export default function ()  {
+    // 홈 페이지
+    homePage();
+    // 경로 검색
+    findPath();
+    // 로그인
+    login();
+
+    sleep(1);
+}
+
+function homePage() {
+    const response = http.get(BASE_URL);
+
+    check(response, {
+        'entered in main page successfully': (res) => res.status === 200
+    });
+}
+
+function login() {
+    const payload = JSON.stringify({
+        email: USERNAME,
+        password: PASSWORD,
+    });
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    let loginRes = http.post(`${BASE_URL}/login/token`, payload, params);
+
+    check(loginRes, {
+        'logged in successfully': (resp) => resp.json('accessToken') !== '',
+    });
+}
+
+function findPath() {
+    const response = http.get(`${BASE_URL}/paths?source=3&target=4`);
+
+    check(response, {
+        'find path successfully': (res) => res.status === 200
+    });
+}
