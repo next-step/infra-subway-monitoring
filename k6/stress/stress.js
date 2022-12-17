4693ef5072f3d196261d666e439670959f38a58d
이성혁 <46282970+seonghyeoklee@users.noreply.github.com>
2022-12-17 13:37:24 +0900

    docs : 부하 테스트 결과 업로드 (#647)

---
+++ k6/stress/stress.js
@@ -0,0 +1,51 @@
+import http from 'k6/http';
+import {check, group, sleep, fail} from 'k6';
+
+export let options = {
+    stages: [
+        {duration: '5m', target: 5},
+        {duration: '5m', target: 10},
+        {duration: '5m', target: 20},
+        {duration: '5m', target: 40},
+        {duration: '5m', target: 80},
+        {duration: '5m', target: 100},
+        {duration: '5m', target: 150},
+        {duration: '5m', target: 200},
+        {duration: '5m', target: 250},
+        {duration: '5m', target: 300},
+    ], thresholds: {
+        http_req_duration: ['p(99)<100'],
+    },
+};
+
+const BASE_URL = 'https://seong.wootecam.o-r.kr/';
+
+function main() {
+    let mainPageRes = http.get(`${BASE_URL}/`);
+    check(mainPageRes, {
+        'Moved to main page': (res) => res.status === 200,
+    });
+}
+
+function path() {
+    let pathPageRes = http.get(`${BASE_URL}/path`);
+    ;
+    check(pathPageRes, {
+        'Moved to path page': (res) => res.status === 200,
+    });
+}
+
+function findPath(authHeaders) {
+    let findPathRes = http.get(`${BASE_URL}/path?source=1&target=50`, authHeaders);
+    check(findPathRes, {
+        'Found path': (res) => res.status === 200,
+    });
+}
+
+export default function () {
+    main();
+    path();
+    findPath();
+
+    sleep(1);
+};
