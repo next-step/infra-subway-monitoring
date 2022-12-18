f2dd502cc5c8ce9b0a25b4df2707e3020752dd7b
seonghyeoklee <dltjdgur327@gmail.com>
2022-12-18 17:09:15 +0900

    feat : 애플리케이션 로그 설정하기

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -68,4 +68,13 @@ public class LineResponse {
     public LocalDateTime getModifiedDate() {
         return modifiedDate;
     }
+
+    @Override
+    public String toString() {
+        return "LineResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
