8a581d71d729f4f032cd55bba2f397f1a173fdd7
soo <yunsoo0510@daum.net>
2022-12-21 20:32:08 +0900

    feat: 애플리케이션 로깅

--- src/main/java/nextstep/subway/PageController.java
+++ src/main/java/nextstep/subway/PageController.java
@@ -1,11 +1,17 @@
 package nextstep.subway;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    private static final Logger log = LoggerFactory.getLogger("file");
+
     @GetMapping(value = {
             "/",
             "/stations",
@@ -17,7 +23,8 @@ public class PageController {
             "/mypage",
             "/mypage/edit",
             "/favorites"}, produces = MediaType.TEXT_HTML_VALUE)
-    public String index() {
+    public String index(HttpServletRequest request) {
+        log.info("page in: {} {}", request.getRemoteAddr(), request.getServletPath());
         return "index";
     }
 }
--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -1,5 +1,7 @@
 package nextstep.subway.map.application;
 
+import static net.logstash.logback.argument.StructuredArguments.*;
+
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.map.domain.SubwayPath;
@@ -7,14 +9,21 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import net.logstash.logback.argument.StructuredArguments;
+
 @Service
 @Transactional
 public class MapService {
+    private static final Logger jsonLog = LoggerFactory.getLogger("json");
+
     private LineService lineService;
     private StationService stationService;
     private PathService pathService;
@@ -26,6 +35,13 @@ public class MapService {
     }
 
     public PathResponse findPath(Long source, Long target) {
+        jsonLog.info("{}", array(
+                        "findPath",
+                        kv("source", source),
+                        kv("target", target)
+                )
+        );
+
         List<Line> lines = lineService.findLines();
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
