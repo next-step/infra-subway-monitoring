ad51e9330178f60a5b657d561e01ac5e54ae5c68
cohi <tkfka156@gmail.com>
2023-02-13 23:14:28 +0900

    json 로그 적용

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -5,6 +5,8 @@ import static net.logstash.logback.argument.StructuredArguments.kv;
 import lombok.extern.slf4j.Slf4j;
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class MapController {
+    private static Logger jsonLog = LoggerFactory.getLogger("json");
     private MapService mapService;
 
     public MapController(MapService mapService) {
@@ -21,6 +24,7 @@ public class MapController {
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
         log.info("{}, {}", kv("출발지", source), kv("도착지", target));
+        jsonLog.info("{}, {}", kv("출발지", source), kv("도착지", target));
         return ResponseEntity.ok(mapService.findPath(source, target));
     }
 }
