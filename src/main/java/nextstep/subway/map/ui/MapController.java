9a3a7eaea7ed9faccdd283f7072d14b0ad010453
cohi <tkfka156@gmail.com>
2023-02-13 22:00:44 +0900

    로그 설정

--- src/main/java/nextstep/subway/map/ui/MapController.java
+++ src/main/java/nextstep/subway/map/ui/MapController.java
@@ -1,12 +1,15 @@
 package nextstep.subway.map.ui;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
+import lombok.extern.slf4j.Slf4j;
 import nextstep.subway.map.application.MapService;
 import nextstep.subway.map.dto.PathResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class MapController {
     private MapService mapService;
@@ -17,6 +20,7 @@ public class MapController {
 
     @GetMapping("/paths")
     public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
+        log.info("{}, {}", kv("출발지", source), kv("도착지", target));
         return ResponseEntity.ok(mapService.findPath(source, target));
     }
 }
